51341a319ff43545dbff693b4d152bf7477a1d74
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-02-21 22:07:56 -0500

    Updated allowable jewelry

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/augment/FriendlyAugment.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/augment/FriendlyAugment.kt
@@ -1,6 +1,7 @@
 package me.fzzyhmstrs.amethyst_imbuement.augment
 
 import me.fzzyhmstrs.amethyst_imbuement.augment.base_augments.PassiveAugment
+import me.fzzyhmstrs.amethyst_imbuement.item.ImbuedJewelryItem
 import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterItem
 import net.minecraft.entity.EquipmentSlot
 import net.minecraft.item.ItemStack
@@ -8,12 +9,14 @@ import net.minecraft.item.ItemStack
 class FriendlyAugment(weight: Rarity, mxLvl: Int = 1, vararg slot: EquipmentSlot): PassiveAugment(weight,mxLvl, *slot) {
 
     override fun isAcceptableItem(stack: ItemStack): Boolean {
-        return (stack.isOf(RegisterItem.IMBUED_HEADBAND))
+        return (stack.item is ImbuedJewelryItem)
     }
 
     override fun acceptableItemStacks(): MutableList<ItemStack> {
         val list = mutableListOf<ItemStack>()
         list.add(ItemStack(RegisterItem.IMBUED_HEADBAND,1))
+        list.add(ItemStack(RegisterItem.IMBUED_AMULET,1))
+        list.add(ItemStack(RegisterItem.IMBUED_RING,1))
         return list
     }
 
