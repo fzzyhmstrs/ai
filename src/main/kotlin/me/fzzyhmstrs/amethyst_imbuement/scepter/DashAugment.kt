990a3e7431b379a7713df8651ba1e222be6f71fc
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-04-19 15:43:58 -0400

    Removing client

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/scepter/DashAugment.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/scepter/DashAugment.kt
@@ -3,7 +3,6 @@ package me.fzzyhmstrs.amethyst_imbuement.scepter
 import me.fzzyhmstrs.amethyst_imbuement.scepter.base_augments.MiscAugment
 import me.fzzyhmstrs.amethyst_imbuement.util.ScepterObject
 import me.fzzyhmstrs.amethyst_imbuement.util.SpellType
-import net.minecraft.client.MinecraftClient
 import net.minecraft.entity.Entity
 import net.minecraft.entity.EquipmentSlot
 import net.minecraft.entity.LivingEntity
@@ -18,6 +17,7 @@ import net.minecraft.util.hit.HitResult
 import net.minecraft.util.math.MathHelper
 import net.minecraft.util.math.Vec3d
 import net.minecraft.world.World
+import java.util.*
 
 class DashAugment(tier: Int, maxLvl: Int, vararg slot: EquipmentSlot): MiscAugment(tier,maxLvl, *slot) {
 
@@ -59,7 +59,7 @@ class DashAugment(tier: Int, maxLvl: Int, vararg slot: EquipmentSlot): MiscAugme
     }
 
     override fun soundEvent(): SoundEvent {
-        return when(MinecraftClient.getInstance().world?.random?.nextInt(3)?:0){
+        return when(Random(124). nextInt(3)){
             0-> SoundEvents.ITEM_TRIDENT_RIPTIDE_1
             1-> SoundEvents.ITEM_TRIDENT_RIPTIDE_2
             2-> SoundEvents.ITEM_TRIDENT_RIPTIDE_3
