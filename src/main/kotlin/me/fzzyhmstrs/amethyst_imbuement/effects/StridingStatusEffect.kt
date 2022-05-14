5f4cfa9896c5c34255548d4b3d613f59c19dd586
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-05-14 19:00:31 -0400

    Overriding update functions to remove useless tick checking from vanilla superclass

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/effects/StridingStatusEffect.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/effects/StridingStatusEffect.kt
@@ -1,8 +1,16 @@
 package me.fzzyhmstrs.amethyst_imbuement.effects
 
+import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.effect.StatusEffect
 import net.minecraft.entity.effect.StatusEffectCategory
 
 class StridingStatusEffect(statusEffectCategory:StatusEffectCategory, i: Int):
     StatusEffect(statusEffectCategory,i) {
+
+    override fun canApplyUpdateEffect(duration: Int, amplifier: Int): Boolean {
+        return false
+    }
+
+    override fun applyUpdateEffect(entity: LivingEntity, amplifier: Int) {
+    }
 }
