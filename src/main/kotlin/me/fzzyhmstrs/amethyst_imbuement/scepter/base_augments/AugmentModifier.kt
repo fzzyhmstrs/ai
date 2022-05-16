e36fae13d2d31a7d984c810e1c96230e068d6c83
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2022-05-16 09:45:38 -0400

    Update AugmentModifier.kt
    adding an xpModifier that lets a modifier increase the xp gain of using a spell.

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/scepter/base_augments/AugmentModifier.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/scepter/base_augments/AugmentModifier.kt
@@ -12,14 +12,17 @@ open class AugmentModifier(
     val manaCostModifier: Int = 0,
     val rangeModifier: Double = 0.0,
     val levelModifier: Int = 0,
+    val xpModifier: XpModifiers? = null,
     val modifierLifespan: Int = 0,
     val secondaryEffect: SecondaryEffect = SecondaryEffect(),
     val spellsToAffect: List<Identifier>? = null,
+    
 ) {
 
     private val hasSpell: Boolean = spellsToAffect != null
     private val hasEffect: Boolean = secondaryEffect.secondaryEffect != null
     private val hasLife: Boolean = modifierLifespan != 0
+    private val hasXp = xpModifier != null
 
     fun hasSpellToAffect(): Boolean{
         return hasSpell
@@ -32,7 +35,13 @@ open class AugmentModifier(
     fun hasLifespan(): Boolean{
         return hasLife
     }
+    
+    fun hasXpMods(): Boolean{
+        return hasXp
+    }
 
     data class SecondaryEffect(val overridesEffect: Boolean = false, val secondaryEffect: ScepterAugment? = null){}
+    
+    data class XpModifiers(val furyXpMod: Int = 0, val witXpMod: Int = 0, val graceXpMod: Int = 0){}
 
 }
