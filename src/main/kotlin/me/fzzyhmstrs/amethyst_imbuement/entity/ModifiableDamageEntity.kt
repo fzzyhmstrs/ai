fab548b303cb69f35a905171d8bcd211f0a27562
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-05-27 22:57:42 -0400

    Adding consumers to the modifiable damage entity interface so augments can pass their consumers to their entities.
    (cherry picked from commit 92ca47b4617321044a3e48b7dd6f55b1c5617f0e)

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/entity/ModifiableDamageEntity.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/entity/ModifiableDamageEntity.kt
@@ -1,9 +1,13 @@
 package me.fzzyhmstrs.amethyst_imbuement.entity
 
+import net.minecraft.entity.LivingEntity
+import java.util.function.Consumer
+
 interface ModifiableDamageEntity {
 
     var damage: Float
     var amplifier: Int
     var range: Double
     var duration: Int
+    var consumers: List<Consumer<List<LivingEntity>>>
 }
