5d95582fe0a4d29c448ca99bbb6049ca7d8c7d0e
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-06-04 19:48:29 -0400

    updated CustomFlavorItem to allow for various namespaces

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/CustomFlavorItem.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/CustomFlavorItem.kt
@@ -1,5 +1,6 @@
 package me.fzzyhmstrs.amethyst_imbuement.item
 
+import me.fzzyhmstrs.amethyst_imbuement.AI
 import net.minecraft.client.item.TooltipContext
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
@@ -8,18 +9,19 @@ import net.minecraft.text.TranslatableText
 import net.minecraft.util.Formatting
 import net.minecraft.world.World
 
-class CustomFlavorItem(settings: Settings, _ttn: String,_glint: Boolean) : Item(settings) {
+class CustomFlavorItem(settings: Settings, nameSpace: String = AI.MOD_ID, flavor: String, glint: Boolean) : Item(settings) {
 
-    private val ttn: String = _ttn
-    private val glint: Boolean = _glint
+    private val ns: String = nameSpace
+    private val ttn: String = flavor
+    private val itemGlint: Boolean = glint
 
     override fun appendTooltip(stack: ItemStack, world: World?, tooltip: MutableList<Text>, context: TooltipContext) {
         super.appendTooltip(stack, world, tooltip, context)
-        tooltip.add(TranslatableText("item.amethyst_imbuement.$ttn.tooltip1").formatted(Formatting.WHITE, Formatting.ITALIC))
+        tooltip.add(TranslatableText("item.$ns.$ttn.tooltip1").formatted(Formatting.WHITE, Formatting.ITALIC))
     }
 
     override fun hasGlint(stack: ItemStack): Boolean {
-        return if (glint) {
+        return if (itemGlint) {
             true
         } else {
             super.hasGlint(stack)
