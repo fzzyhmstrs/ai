7d18a8c29ec683346b49714504b8964c8d81b4b2
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-06-12 11:39:19 -0400

    Tweaking player-tracking particle methods

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/ScepterItem.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/ScepterItem.kt
@@ -307,8 +307,19 @@ open class ScepterItem(material: ToolMaterial, settings: Settings, vararg defaul
         }
 
         private fun scepterParticlePos(pos: Vec3d, width: Float, yaw: Float, perspective: Perspective, fov: Double): Vec3d{
-            val offset: Vec3d = when(perspective){
+            val offset: Vec3d = scepterOffset(perspective, fov)
+            return playerParticlePos(pos, width, yaw, offset)
+        }
 
+        fun playerParticlePos(pos: Vec3d, width: Float, yaw: Float, offset: Vec3d): Vec3d{
+            val posX = pos.x - (width + offset.x) * 0.5 * MathHelper.sin(yaw * (Math.PI.toFloat() / 180)) - offset.z * MathHelper.cos(yaw * (Math.PI.toFloat() / 180))
+            val posY = pos.y + offset.y
+            val posZ = pos.z + (width + offset.x) * 0.5 * MathHelper.cos(yaw * (Math.PI.toFloat() / 180)) - offset.z * MathHelper.sin(yaw * (Math.PI.toFloat() / 180))
+            return Vec3d(posX, posY, posZ)
+        }
+
+        fun scepterOffset(perspective: Perspective, fov: Double): Vec3d{
+            return when(perspective){
                 Perspective.FIRST_PERSON -> {
                     val fpx = MathHelper.lerpFromProgress(fov,30.0,110.0,1.4,-0.1)
                     Vec3d(fpx,-0.13,0.6)
@@ -320,10 +331,6 @@ open class ScepterItem(material: ToolMaterial, settings: Settings, vararg defaul
                     Vec3d(0.8, -0.5, 0.5)
                 }
             }
-            val posX = pos.x - (width + offset.x) * 0.5 * MathHelper.sin(yaw * (Math.PI.toFloat() / 180)) - offset.z * MathHelper.cos(yaw * (Math.PI.toFloat() / 180))
-            val posY = pos.y + offset.y
-            val posZ = pos.z + (width + offset.x) * 0.5 * MathHelper.cos(yaw * (Math.PI.toFloat() / 180)) - offset.z * MathHelper.sin(yaw * (Math.PI.toFloat() / 180))
-            return Vec3d(posX, posY, posZ)
         }
 
         fun writeDefaultNbt(stack: ItemStack){
