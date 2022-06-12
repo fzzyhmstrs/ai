1dd34ab0239dd9752f98e76bd03802c38f0f29d8
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-06-12 11:38:55 -0400

    Implemented new registration code for items with custom models and entities.
    (cherry picked from commit 52dcf09322cc36d6b99add9a80e94dd750eac7b5)

---
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/model/GlisteringTridentItemEntityRenderer.kt
@@ -0,0 +1,39 @@
+package me.fzzyhmstrs.amethyst_imbuement.model
+
+import me.fzzyhmstrs.amethyst_imbuement.model.GlisteringTridentEntityModel.Companion.TEXTURE
+import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterItem
+import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterItemModel
+import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry
+import net.minecraft.client.render.VertexConsumerProvider
+import net.minecraft.client.render.item.ItemRenderer
+import net.minecraft.client.render.model.json.ModelTransformation
+import net.minecraft.client.util.math.MatrixStack
+import net.minecraft.item.ItemStack
+
+object GlisteringTridentItemEntityRenderer: BuiltinItemRendererRegistry.DynamicItemRenderer {
+
+    private val modelLoader by lazy {
+        RegisterItemModel.getEntityModelLoader(RegisterItem.GLISTERING_TRIDENT)
+    }
+
+    override fun render(
+        stack: ItemStack,
+        mode: ModelTransformation.Mode,
+        matrices: MatrixStack,
+        vertexConsumers: VertexConsumerProvider,
+        light: Int,
+        overlay: Int
+    ) {
+
+        val renderModel = modelLoader.getModel()
+        matrices.push()
+        matrices.scale(1.0f, -1.0f, -1.0f)
+        val block = ItemRenderer.getDirectItemGlintConsumer(
+            vertexConsumers, renderModel.getLayer(
+                TEXTURE
+            ), false, stack.hasGlint()
+        )
+        renderModel.render(matrices, block, light, overlay, 1.0f, 1.0f, 1.0f, 1.0f)
+        matrices.pop()
+    }
+}
---
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/registry/RegisterItemModel.kt
@@ -0,0 +1,150 @@
+package me.fzzyhmstrs.amethyst_imbuement.registry
+
+import me.fzzyhmstrs.amethyst_imbuement.AI
+import me.fzzyhmstrs.amethyst_imbuement.model.GlisteringTridentEntityModel
+import me.fzzyhmstrs.amethyst_imbuement.model.GlisteringTridentItemEntityRenderer
+import net.fabricmc.api.EnvType
+import net.fabricmc.api.Environment
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking
+import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.model.Model
+import net.minecraft.client.model.ModelPart
+import net.minecraft.client.render.entity.model.EntityModelLayer
+import net.minecraft.client.render.entity.model.EntityModelLoader
+import net.minecraft.client.render.model.json.ModelTransformation
+import net.minecraft.client.util.ModelIdentifier
+import net.minecraft.item.Item
+import net.minecraft.resource.ReloadableResourceManagerImpl
+import net.minecraft.resource.ResourceManager
+import net.minecraft.resource.SynchronousResourceReloader
+import java.util.*
+import java.util.function.Function
+import kotlin.NoSuchElementException
+import kotlin.collections.HashMap
+import kotlin.reflect.KClass
+import kotlin.reflect.KProperty0
+import kotlin.reflect.KType
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.full.starProjectedType
+import kotlin.reflect.full.withNullability
+import kotlin.reflect.jvm.internal.impl.load.java.structure.JavaClass
+import kotlin.reflect.jvm.javaField
+import kotlin.reflect.typeOf
+
+@Environment(value = EnvType.CLIENT)
+object RegisterItemModel: SynchronousResourceReloader {
+
+    private val fallbackId = ModelIdentifier("minecraft:trident_in_hand#inventory")
+    private val modelIdMap: HashMap<Item, ModelIdentifierPerModes> = HashMap()
+    private val entityModelMap: HashMap<Item,CustomItemEntityModelLoader> = HashMap()
+    private val entityModelLoader: EntityModelLoader by lazy { MinecraftClient.getInstance().entityModelLoader }
+
+    var GLISTERING_TRIDENT_MODEL: Model? = null
+
+    override fun reload(manager: ResourceManager) {
+        entityModelMap.forEach {
+            it.value.reload()
+        }
+    }
+
+    fun registerAll(){
+        registerReloader()
+        val modelsPerMode = ModelIdentifierPerModes(ModelIdentifier(AI.MOD_ID + ":glistering_trident#inventory"))
+            .withHeld(ModelIdentifier(AI.MOD_ID + ":glistering_trident_in_hand#inventory"))
+        registerItemModelId(RegisterItem.GLISTERING_TRIDENT, modelsPerMode)
+        registerItemEntityModel(RegisterItem.GLISTERING_TRIDENT,
+            GlisteringTridentItemEntityRenderer,
+            RegisterRenderer.GLISTERING_TRIDENT,
+            GlisteringTridentEntityModel::class.java)
+    }
+
+    fun registerItemModelId(item: Item, models: ModelIdentifierPerModes){
+        if (modelIdMap.containsKey(item)){
+            throw IllegalStateException("Item ${item.name} already present in ItemModelRegistry")
+        }
+        modelIdMap[item] = models
+    }
+
+    fun registerItemEntityModel(item: Item , renderer: BuiltinItemRendererRegistry.DynamicItemRenderer, layer: EntityModelLayer , classType : Class<out Model>){
+        entityModelMap[item] = CustomItemEntityModelLoader(layer, classType)
+        BuiltinItemRendererRegistry.INSTANCE.register(item, renderer)
+    }
+
+    private fun registerReloader(){
+        ClientLifecycleEvents.CLIENT_STARTED.register{
+                client: MinecraftClient -> (client.resourceManager as ReloadableResourceManagerImpl).registerReloader(this)
+        }
+    }
+
+    fun itemHasCustomModel(item: Item): Boolean{
+        return (modelIdMap.containsKey(item))
+    }
+
+    fun getModel(item: Item, mode: ModelTransformation.Mode): ModelIdentifier{
+        return modelIdMap[item]?.getIdFromMode(mode) ?: fallbackId
+    }
+
+    fun getEntityModelLoader(item: Item): CustomItemEntityModelLoader{
+        return entityModelMap[item]?:throw NoSuchElementException("Item ${item.name} not present in model loader registry.")
+    }
+
+    class ModelIdentifierPerModes(private val defaultId: ModelIdentifier){
+        private val modeMap: EnumMap<ModelTransformation.Mode,ModelIdentifier> = EnumMap(ModelTransformation.Mode::class.java)
+
+        fun with(mode: ModelTransformation.Mode, modelId: ModelIdentifier): ModelIdentifierPerModes{
+            modeMap[mode] = modelId
+            return this
+        }
+        fun withGuiGroundFixed(modelId: ModelIdentifier): ModelIdentifierPerModes{
+            modeMap[ModelTransformation.Mode.GUI] = modelId
+            modeMap[ModelTransformation.Mode.FIXED] = modelId
+            modeMap[ModelTransformation.Mode.GROUND] = modelId
+            return this
+        }
+        fun withFirstHeld(modelId: ModelIdentifier): ModelIdentifierPerModes {
+            modeMap[ModelTransformation.Mode.FIRST_PERSON_RIGHT_HAND] = modelId
+            modeMap[ModelTransformation.Mode.FIRST_PERSON_LEFT_HAND] = modelId
+            return this
+        }
+        fun withThirdHeld(modelId: ModelIdentifier): ModelIdentifierPerModes {
+            modeMap[ModelTransformation.Mode.THIRD_PERSON_RIGHT_HAND] = modelId
+            modeMap[ModelTransformation.Mode.THIRD_PERSON_LEFT_HAND] = modelId
+            return this
+        }
+        fun withHeld(modelId: ModelIdentifier): ModelIdentifierPerModes{
+            modeMap[ModelTransformation.Mode.FIRST_PERSON_RIGHT_HAND] = modelId
+            modeMap[ModelTransformation.Mode.FIRST_PERSON_LEFT_HAND] = modelId
+            modeMap[ModelTransformation.Mode.THIRD_PERSON_RIGHT_HAND] = modelId
+            modeMap[ModelTransformation.Mode.THIRD_PERSON_LEFT_HAND] = modelId
+            return this
+        }
+
+        fun getIdFromMode(mode: ModelTransformation.Mode): ModelIdentifier{
+            return modeMap[mode] ?: defaultId
+        }
+    }
+
+    class CustomItemEntityModelLoader(private val layer: EntityModelLayer, private val classType: Class<out Model>){
+
+        private lateinit var model: Model
+
+        fun reload(){
+            model = internalReload()
+        }
+
+        fun getModel(): Model{
+            if (!this::model.isInitialized){
+                model = internalReload()
+            }
+            return model
+        }
+
+        private fun internalReload(): Model{
+            val constructor = classType.getConstructor(ModelPart::class.java)
+            val modelPart = entityModelLoader.getModelPart(layer)
+            return constructor.newInstance(modelPart)
+        }
+    }
+}
