6a9c2ee9ea22c59484772899c32678308f8153a8
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-06-26 11:57:46 -0400

    Transitioning utils

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/util/ImbuingRecipeSerializer.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/util/ImbuingRecipeSerializer.kt
@@ -3,8 +3,9 @@ package me.fzzyhmstrs.amethyst_imbuement.util
 import com.google.gson.Gson
 import com.google.gson.JsonObject
 import com.google.gson.JsonSyntaxException
+import me.fzzyhmstrs.amethyst_core.coding_util.AugmentDatapoint
+import me.fzzyhmstrs.amethyst_core.scepter_util.ScepterHelper
 import me.fzzyhmstrs.amethyst_imbuement.AI
-import me.fzzyhmstrs.amethyst_imbuement.scepter.base_augments.ScepterObject
 import net.minecraft.network.PacketByteBuf
 import net.minecraft.recipe.Ingredient
 import net.minecraft.recipe.RecipeSerializer
@@ -46,15 +47,15 @@ object ImbuingRecipeSerializer: RecipeSerializer<ImbuingRecipe> {
         }
         if (augmentA != ""){
             val augId = Identifier(augmentA).toString()
-            if (ScepterObject.checkAugmentStat(augId)){
-                val type = ScepterObject.getAugmentType(augId)
-                val cooldown = ScepterObject.getAugmentCooldown(augId)
-                val manaCost = ScepterObject.getAugmentManaCost(augId)
-                val minLevel = ScepterObject.getAugmentMinLvl(augId)
-                val bookOfLoreTier = ScepterObject.getAugmentTier(augId)
-                val keyItem = ScepterObject.getAugmentItem(augId)
-                val datapoint = ScepterObject.AugmentDatapoint(type,cooldown,manaCost,minLevel,costA,bookOfLoreTier,keyItem)
-                ScepterObject.registerAugmentStat(augId,datapoint,true)
+            if (ScepterHelper.checkAugmentStat(augId)){
+                val type = ScepterHelper.getAugmentType(augId)
+                val cooldown = ScepterHelper.getAugmentCooldown(augId)
+                val manaCost = ScepterHelper.getAugmentManaCost(augId)
+                val minLevel = ScepterHelper.getAugmentMinLvl(augId)
+                val bookOfLoreTier = ScepterHelper.getAugmentTier(augId)
+                val keyItem = ScepterHelper.getAugmentItem(augId)
+                val datapoint = AugmentDatapoint(type,cooldown,manaCost,minLevel,costA,bookOfLoreTier,keyItem)
+                ScepterHelper.registerAugmentStat(augId,datapoint,true)
             }
         }
         val countA: Int = if (recipeJson.countA == 0){ 1 } else{ recipeJson.countA?:1 }
