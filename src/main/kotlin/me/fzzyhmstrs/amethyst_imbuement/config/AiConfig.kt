b28ff1883846ac763f64cb5f78c67f3bbd6ea856
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-06-26 10:07:51 -0400

    Transitioning the config

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/config/AiConfig.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/config/AiConfig.kt
@@ -1,8 +1,12 @@
 package me.fzzyhmstrs.amethyst_imbuement.config
 
 import com.google.gson.GsonBuilder
+import me.fzzyhmstrs.amethyst_core.misc_util.SyncedConfigHelper
+import me.fzzyhmstrs.amethyst_core.misc_util.SyncedConfigHelper.gson
+import me.fzzyhmstrs.amethyst_core.misc_util.SyncedConfigHelper.readOrCreate
+import me.fzzyhmstrs.amethyst_core.misc_util.SyncedConfigHelper.readOrCreateUpdated
+import me.fzzyhmstrs.amethyst_core.scepter_util.ScepterAugment
 import me.fzzyhmstrs.amethyst_imbuement.AI
-import me.fzzyhmstrs.amethyst_imbuement.scepter.base_augments.ScepterObject
 import me.fzzyhmstrs.amethyst_imbuement.tool.ScepterLvl2ToolMaterial
 import me.fzzyhmstrs.amethyst_imbuement.tool.ScepterLvl3ToolMaterial
 import me.fzzyhmstrs.amethyst_imbuement.tool.ScepterLvl1ToolMaterial
@@ -11,10 +15,7 @@ import net.minecraft.network.PacketByteBuf
 import net.minecraft.util.Identifier
 import java.io.File
 
-object AiConfig {
-
-    val gson = GsonBuilder().setPrettyPrinting().create()
-    const val augmentVersion = "_v0"
+object AiConfig: SyncedConfigHelper.SyncedConfig {
 
     var scepters: Scepters
     var altars: Altars
@@ -34,97 +35,14 @@ object AiConfig {
 
     fun initConfig(){}
 
-    @Deprecated("moving to amethyst_core")
-    inline fun <reified T> readOrCreate(file: String, child: String = "", configClass: () -> T): T {
-        val (dir,dirCreated) = makeDir(child)
-        if (!dirCreated) {
-            return configClass()
-        }
-        val f = File(dir, file)
-        try {
-            if (f.exists()) {
-                return gson.fromJson(f.readLines().joinToString(""), T::class.java)
-            } else if (!f.createNewFile()) {
-                println("Failed to create default config file ($file), using default config.")
-            } else {
-                f.writeText(gson.toJson(configClass()))
-            }
-            return configClass()
-        } catch (e: Exception) {
-            println("Failed to read config file! Using default values: " + e.message)
-            return configClass()
-        }
-    }
-
-    @Deprecated("moving to amethyst_core")
-    @Suppress("UNUSED_PARAMETER")
-    inline fun <reified T, reified P> readOrCreateUpdated(file: String, previous: String, child: String = "", configClass: () -> T, previousClass: () -> P): T{
-        val (dir,dirCreated) = makeDir(child)
-        if (!dirCreated) {
-            return configClass()
-        }
-        val p = File(dir, previous)
-        try {
-            if (p.exists()) {
-                val previousConfig = gson.fromJson(p.readLines().joinToString(""), P::class.java)
-                if (previousConfig is OldClass){
-                    val newClass = previousConfig.generateNewClass()
-                    if (newClass !is T){
-                        throw RuntimeException("Old config class is not returning the proper new config class: ${P::class.simpleName} is returning ${newClass.javaClass.simpleName}; expected ${T::class.simpleName}")
-                    } else {
-                        val f = File(dir,file)
-                        if (f.exists()){
-                            p.delete() //attempts to delete the now useless old config version file
-                            return gson.fromJson(f.readLines().joinToString(""), T::class.java)
-                        } else if (!f.createNewFile()){
-                            //don't delete old file if the new one can't be generated to take its place
-                            println("Failed to create new config file ($file), using old config with new defaults.")
-                        } else {
-                            p.delete() //attempts to delete the now useless old config version file
-                            f.writeText(gson.toJson(newClass))
-                        }
-                        return newClass
-                    }
-                } else {
-                    throw RuntimeException("Old config not properly set up as an OldConfig: ${P::class.simpleName}")
-                }
-            } else {
-                return readOrCreate(file, configClass = configClass)
-            }
-        } catch (e: Exception) {
-            println("Failed to read config file! Using default values: " + e.message)
-            return configClass()
-        }
-    }
-
-    fun configAugment(file: String, configClass: AugmentStats): AugmentStats{
-        return readOrCreate(file,"augments") {configClass}
-    }
-
-    @Deprecated("moving to amethyst_core")
-    fun makeDir(child:String = ""): Pair<File,Boolean>{
-        val dir = if (child != ""){
-            File(File(FabricLoader.getInstance().configDir.toFile(), AI.MOD_ID),child)
-        } else {
-            File(FabricLoader.getInstance().configDir.toFile(), AI.MOD_ID)
-        }
-        if (!dir.exists() && !dir.mkdirs()) {
-            println("Could not create directory, using default configs.")
-            return Pair(dir,false)
-        }
-        return Pair(dir,true)
-    }
-
-    @Deprecated("moving to amethyst_core")
-    fun writeToClient(buf:PacketByteBuf){
+    override fun writeToClient(buf:PacketByteBuf){
         val gson = GsonBuilder().create()
         buf.writeString(gson.toJson(scepters))
         buf.writeString(gson.toJson(altars))
         buf.writeString(gson.toJson(colors))
     }
 
-    @Deprecated("moving to amethyst_core")
-    fun readFromServer(buf:PacketByteBuf){
+    override fun readFromServer(buf:PacketByteBuf){
         scepters = gson.fromJson(buf.readString(),Scepters::class.java)
         altars = gson.fromJson(buf.readString(),Altars::class.java)
         colors = gson.fromJson(buf.readString(),Colors::class.java)
@@ -146,13 +64,6 @@ object AiConfig {
         var imbuingTableDifficultyModifier: Float = 1.0F
         var altarOfExperienceBaseLevels: Int = 35
         var altarOfExperienceCandleLevelsPer: Int = 5
-    }
-
-    class AugmentStats {
-        var id: String = ScepterObject.fallbackAugment
-        var cooldown: Int = 20
-        var manaCost: Int = 2
-        var minLvl: Int = 1
     }
 
     class Colors{
@@ -226,9 +137,9 @@ object AiConfig {
         )
     }
 
-    class ColorsV0: OldClass{
+    class ColorsV0: SyncedConfigHelper.OldClass{
 
-        var defaultColorMap: Map<String,String> = mapOf(
+        private var defaultColorMap: Map<String,String> = mapOf(
             "minecraft:diamond_ore" to "#1ED0D6",
             "minecraft:deepslate_diamond_ore" to "#1ED0D6",
             "minecraft:coal_ore" to "#2A2A2A",
@@ -272,7 +183,7 @@ object AiConfig {
         }
     }
 
-    class AltarsV0: OldClass {
+    class AltarsV0: SyncedConfigHelper.OldClass {
         var disenchantLevelCosts: List<Int> = listOf(3, 5, 9, 15, 23)
         var disenchantBaseDisenchantsAllowed: Int = 1
         var imbuingTableEnchantingEnabled: Boolean = true
@@ -290,12 +201,5 @@ object AiConfig {
             altars.altarOfExperienceCandleLevelsPer = altarOfExperienceCandleLevelsPer
             return altars
         }
-    }
-
-    @Deprecated("moving to amethyst_core")
-    interface OldClass{
-
-        fun generateNewClass(): Any
-
     }
 }
