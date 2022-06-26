7ae11c01ddfcad74f84f5625830bbb102b3c5981
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-06-26 12:06:45 -0400

    Updating the main entrypoint file post-transition

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/amethyst_imbuement.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/amethyst_imbuement.kt
@@ -4,6 +4,7 @@ package me.fzzyhmstrs.amethyst_imbuement
 
 import me.fzzyhmstrs.amethyst_imbuement.config.AiConfig
 import me.fzzyhmstrs.amethyst_imbuement.registry.*
+import me.fzzyhmstrs.amethyst_imbuement.scepter.base_augments.PlaceItemAugment
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
@@ -27,8 +28,7 @@ object AI: ModInitializer {
         RegisterStatus.registerAll()
         RegisterVillager.registerAll()
         RegisterRecipe.registerAll()
-        RegisterEvent.registerAll()
-        RegisterModifier.registerAll()
+        PlaceItemAugment.registerServer()
     }
 
     fun aiRandom(): Random{
@@ -43,7 +43,6 @@ object AIClient: ClientModInitializer{
         RegisterRenderer.registerAll()
         RegisterScreen.registerAll()
         RegisterKeybind.registerAll()
-        RegisterEvent.registerClient()
         RegisterItemModel.registerAll()
     }
 
