f8db6f2e801cb6ef2da3e65b507a411ada4067c9
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-06-26 22:51:20 -0400

    messing with custom flavor items

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/ImbuedJewelryItem.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/ImbuedJewelryItem.kt
@@ -1,17 +1,38 @@
 package me.fzzyhmstrs.amethyst_imbuement.item
 
+import com.google.common.collect.Multimap
 import dev.emi.trinkets.api.SlotReference
 import me.fzzyhmstrs.amethyst_core.item_util.AbstractAugmentJewelryItem
 import me.fzzyhmstrs.amethyst_core.registry.EventRegistry
+import me.fzzyhmstrs.amethyst_imbuement.AI
 import me.fzzyhmstrs.amethyst_imbuement.augment.ShieldingAugment
 import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterEnchantment
 import net.minecraft.enchantment.EnchantmentHelper
 import net.minecraft.entity.Entity
 import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.attribute.EntityAttribute
+import net.minecraft.entity.attribute.EntityAttributeModifier
+import net.minecraft.entity.attribute.EntityAttributes
 import net.minecraft.item.ItemStack
+import net.minecraft.util.Identifier
 import net.minecraft.world.World
+import java.util.*
 
-open class ImbuedJewelryItem(settings: Settings,_ttn: String):AbstractAugmentJewelryItem(settings, _ttn) {
+open class ImbuedJewelryItem(settings: Settings, flavor: String):AbstractAugmentJewelryItem(settings, Identifier(AI.MOD_ID,flavor)) {
+
+    override fun getModifiers(
+        stack: ItemStack,
+        slot: SlotReference,
+        entity: LivingEntity,
+        uuid: UUID
+    ): Multimap<EntityAttribute, EntityAttributeModifier> {
+        val modifiers = super.getModifiers(stack, slot, entity, uuid)
+        modifiers.put(
+            EntityAttributes.GENERIC_MOVEMENT_SPEED,
+            EntityAttributeModifier(uuid, "amethyst_imbuement:movement_speed", 0.03, EntityAttributeModifier.Operation.MULTIPLY_TOTAL)
+        )
+        return modifiers
+    }
 
     override fun onEquip(stack: ItemStack, slot: SlotReference, entity: LivingEntity) {
         if (entity.world.isClient()) return
