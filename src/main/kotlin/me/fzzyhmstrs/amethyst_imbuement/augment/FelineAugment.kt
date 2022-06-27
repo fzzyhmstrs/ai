55f05950d98e2ffd7a9aaa3938d0171614618eb3
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-06-26 23:16:57 -0400

    removing direct dependency on Trinkets

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/augment/FelineAugment.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/augment/FelineAugment.kt
@@ -1,9 +1,8 @@
 package me.fzzyhmstrs.amethyst_imbuement.augment
 
-import dev.emi.trinkets.api.TrinketsApi
+import me.fzzyhmstrs.amethyst_core.item_util.TrinketUtil
 import me.fzzyhmstrs.amethyst_imbuement.augment.base_augments.PassiveAugment
 import me.fzzyhmstrs.amethyst_imbuement.item.ImbuedJewelryItem
-import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterEnchantment
 import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterEnchantment.FELINE
 import net.minecraft.enchantment.EnchantmentHelper
 import net.minecraft.entity.EquipmentSlot
@@ -13,14 +12,12 @@ import net.minecraft.item.ItemStack
 class FelineAugment(weight: Rarity,mxLvl: Int = 1, vararg slot: EquipmentSlot): PassiveAugment(weight,mxLvl, *slot) {
 
     override fun specialEffect(user: LivingEntity, level: Int, stack: ItemStack): Boolean {
-        val comp = TrinketsApi.getTrinketComponent(user)
-        if (comp.isPresent) {
-            val items = comp.get().allEquipped
-            for (slot in items) {
-                if (slot.right.item is ImbuedJewelryItem) {
-                    if (EnchantmentHelper.getLevel(FELINE, slot.right) > 0) {
-                        return true
-                    }
+
+        val stacks = TrinketUtil.getTrinketStacks(user)
+        stacks.forEach {
+            if (it.item is ImbuedJewelryItem){
+                if (EnchantmentHelper.getLevel(FELINE, it) > 0) {
+                    return true
                 }
             }
         }
--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/augment/FriendlyAugment.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/augment/FriendlyAugment.kt
@@ -1,6 +1,6 @@
 package me.fzzyhmstrs.amethyst_imbuement.augment
 
-import dev.emi.trinkets.api.TrinketsApi
+import me.fzzyhmstrs.amethyst_core.item_util.TrinketUtil
 import me.fzzyhmstrs.amethyst_imbuement.augment.base_augments.PassiveAugment
 import me.fzzyhmstrs.amethyst_imbuement.item.ImbuedJewelryItem
 import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterEnchantment.FRIENDLY
@@ -13,14 +13,12 @@ import net.minecraft.item.ItemStack
 class FriendlyAugment(weight: Rarity, mxLvl: Int = 1, vararg slot: EquipmentSlot): PassiveAugment(weight,mxLvl, *slot) {
 
     override fun specialEffect(user: LivingEntity, level: Int, stack: ItemStack): Boolean {
-        val comp = TrinketsApi.getTrinketComponent(user)
-        if (comp.isPresent) {
-            val items = comp.get().allEquipped
-            for (slot in items) {
-                if (slot.right.item is ImbuedJewelryItem) {
-                    if (EnchantmentHelper.getLevel(FRIENDLY, slot.right) > 0) {
-                        return true
-                    }
+
+        val stacks = TrinketUtil.getTrinketStacks(user)
+        stacks.forEach {
+            if (it.item is ImbuedJewelryItem){
+                if (EnchantmentHelper.getLevel(FRIENDLY, it) > 0) {
+                    return true
                 }
             }
         }
