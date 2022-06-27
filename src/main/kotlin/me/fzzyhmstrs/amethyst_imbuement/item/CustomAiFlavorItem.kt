8c25ab4a6f97cb9df07ebf869a0956c17e606802
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-06-26 22:51:20 -0400

    messing with custom flavor items

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/CustomAiFlavorItem.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/CustomAiFlavorItem.kt
@@ -2,7 +2,8 @@ package me.fzzyhmstrs.amethyst_imbuement.item
 
 import me.fzzyhmstrs.amethyst_core.item_util.CustomFlavorItem
 import me.fzzyhmstrs.amethyst_imbuement.AI
+import net.minecraft.util.Identifier
 
-class CustomAiFlavorItem(settings: Settings, flavor: String, glint: Boolean, nameSpace: String = AI.MOD_ID) : CustomFlavorItem(settings, flavor, glint, nameSpace) {
+class CustomAiFlavorItem(settings: Settings, flavor: String, glint: Boolean, nameSpace: String = AI.MOD_ID) : CustomFlavorItem(settings, glint, Identifier(nameSpace, flavor)) {
     //passing the AI namespace
 }
