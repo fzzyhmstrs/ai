751680a863a40d9c9cf2420c7cc35158abe05fea
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-06-29 19:26:28 -0400

    Re-transitioning items after library rework

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/BookOfMythosItem.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/BookOfMythosItem.kt
@@ -1,5 +1,6 @@
 package me.fzzyhmstrs.amethyst_imbuement.item
 
+import me.fzzyhmstrs.amethyst_core.item_util.AbstractAugmentBookItem
 import me.fzzyhmstrs.amethyst_core.nbt_util.Nbt
 import me.fzzyhmstrs.amethyst_core.nbt_util.NbtKeys
 import me.fzzyhmstrs.amethyst_core.scepter_util.LoreTier
@@ -13,19 +14,6 @@ import net.minecraft.util.Hand
 import net.minecraft.util.TypedActionResult
 import net.minecraft.world.World
 
-class BookOfMythosItem(settings: Settings, _ttn: String, _glint: Boolean) : BookOfLoreItem(settings, _ttn, _glint) {
-
-    override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
-        val stack = user.getStackInHand(hand)
-        if (world !is ServerWorld) return TypedActionResult.fail(stack)
-        val nbt = stack.orCreateNbt
-        if(!nbt.contains(NbtKeys.LORE_KEY.str())){
-            val nbtTemp = ScepterHelper.bookOfLoreNbtGenerator(LoreTier.HIGH_TIER)
-            val enchant = Nbt.readStringNbt(NbtKeys.LORE_KEY.str(),nbtTemp)
-            Nbt.writeStringNbt(NbtKeys.LORE_KEY.str(),enchant,nbt)
-            world.playSound(null,user.blockPos, SoundEvents.ITEM_BOOK_PAGE_TURN, SoundCategory.NEUTRAL,0.7f,1.0f)
-            return TypedActionResult.success(stack)
-        }
-        return TypedActionResult.pass(stack)
-    }
+class BookOfMythosItem(settings: Settings) : AbstractAugmentBookItem(settings) {
+    override val loreTier: LoreTier = LoreTier.HIGH_TIER
 }
