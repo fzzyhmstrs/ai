f31dc012789205a9145e26ed112c0ccf1daa20c7
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-07-05 17:11:22 -0400

    fixing up for rebuild

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,9 +35,6 @@ repositories {
             includeGroup("maven.modrinth")
         }
     }
-    flatDir {
-        dirs("F:\\Documents\\Mod Libraries\\ac\\build\\libs")
-    }
     mavenCentral()
 }
 dependencies {
@@ -57,8 +54,6 @@ dependencies {
     modCompileOnly("me.shedaniel:RoughlyEnoughItems-api-fabric:9.0.491")
     modRuntimeOnly("me.shedaniel:RoughlyEnoughItems-fabric:9.0.491")
 
-    modImplementation(":amethyst_core-0.1.0+1.19")
-
     modImplementation("dev.emi:trinkets:3.4.0"){
         exclude("net.fabricmc.fabric-api")
     }
@@ -73,6 +68,11 @@ dependencies {
         exclude("net.fabricmc.fabric-api")
     }
     include("maven.modrinth:Wd844r7Q:1.19-01")
+
+    modImplementation("maven.modrinth:amethyst-core:0.1.0+1.19"){
+        exclude("net.fabricmc.fabric-api")
+    }
+    include("maven.modrinth:amethyst-core:0.1.0+1.19")
 
     modImplementation("maven.modrinth:coloredglowlib:1.3.4"){
         exclude("net.fabricmc.fabric-api")
