b9cdec5021fb01a9dead893bb59057e755c919c2
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-07-09 12:47:42 -0400

    put the modrinth import back into the build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,10 +72,10 @@ dependencies {
     }
     include("maven.modrinth:Wd844r7Q:1.19-01")
 
-    modImplementation(":amethyst_core-0.1.1+1.19"){
+    modImplementation("maven.modrinth:amethyst-core:0.1.1+1.19"){
         exclude("net.fabricmc.fabric-api")
     }
-    include(":amethyst_core-0.1.1+1.19")
+    include("maven.modrinth:amethyst-core:0.1.1+1.19")
 
     modImplementation("maven.modrinth:coloredglowlib:1.3.4"){
         exclude("net.fabricmc.fabric-api")
