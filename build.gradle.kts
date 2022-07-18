281c80e8de41929a4fdd277172aa1536863239a8
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-07-18 17:29:14 -0400

    updated AC version to 0.2.0+1.18.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,10 +70,10 @@ dependencies {
     }
     include("maven.modrinth:Wd844r7Q:1.18.2-02")
 
-    modImplementation("maven.modrinth:amethyst-core:0.1.1+1.18.2"){
+    modImplementation("maven.modrinth:amethyst-core:0.2.0+1.18.2"){
         exclude("net.fabricmc.fabric-api")
     }
-    include("maven.modrinth:amethyst-core:0.1.1+1.18.2")
+    include("maven.modrinth:amethyst-core:0.2.0+1.18.2")
 
     modImplementation("maven.modrinth:coloredglowlib:1.3.0"){
         exclude("net.fabricmc.fabric-api")
