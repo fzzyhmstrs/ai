43725c5b8b2c26c2aa16daca4dd307cc0b08d6ac
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-07-20 15:53:07 -0400

    conflict resolution for carpet-fix onEntityLanded mixin

--- src/main/java/me/fzzyhmstrs/amethyst_imbuement/mixins/EntityMixin.java
+++ src/main/java/me/fzzyhmstrs/amethyst_imbuement/mixins/EntityMixin.java
@@ -29,52 +29,6 @@ public abstract class EntityMixin {
 
     @Shadow public abstract boolean equals(Object o);
 
-/*    @Redirect(method = "fall", at = @At(value = "INVOKE", target = "net/minecraft/block/Block.onLandedUpon (Lnet/minecraft/world/World;Lnet/minecraft/block/BlockState;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/Entity;F)V"))
-    private void onSlimedUpon(Block instance, World world, BlockState state, BlockPos pos, Entity entity, float fallDistance){
-        if (!(entity instanceof PlayerEntity playerEntity)){
-            instance.onLandedUpon(world, state, pos, entity, fallDistance);
-            return;
-        }
-        if (playerEntity.getEquippedStack(EquipmentSlot.FEET).isEmpty()){
-            instance.onLandedUpon(world, state, pos, entity, fallDistance);
-            return;
-        }
-        if (EnchantmentHelper.getLevel(RegisterEnchantment.INSTANCE.getSLIMY(),playerEntity.getEquippedStack(EquipmentSlot.FEET)) > 0){
-            Block slimeBlock = Blocks.SLIME_BLOCK;
-            slimeBlock.onLandedUpon(world, state, pos, entity, fallDistance);
-            entity.world.playSound(null,entity.getBlockPos(), SoundEvents.BLOCK_SLIME_BLOCK_BREAK, SoundCategory.BLOCKS,0.6f,1.0f);
-        } else {
-            instance.onLandedUpon(world, state, pos, entity, fallDistance);
-        }
-    }*/
-
-/*    @Redirect(method = "move", at = @At(value = "INVOKE", target = "net/minecraft/block/Block.onEntityLand (Lnet/minecraft/world/BlockView;Lnet/minecraft/entity/Entity;)V"))
-    private void slimyBounce(Block instance, BlockView world, Entity entity){
-        if (!(entity instanceof PlayerEntity playerEntity)){
-            instance.onEntityLand(world,entity);
-            return;
-        }
-        if (playerEntity.getEquippedStack(EquipmentSlot.FEET).isEmpty()){
-            instance.onEntityLand(world,entity);
-            return;
-        }
-        if (EnchantmentHelper.getLevel(RegisterEnchantment.INSTANCE.getMULTI_JUMP(),playerEntity.getEquippedStack(EquipmentSlot.FEET)) > 0){
-            ((PlayerEntity) entity).removeStatusEffect(RegisterStatus.INSTANCE.getLEAPT());
-        }
-
-        if (EnchantmentHelper.getLevel(RegisterEnchantment.INSTANCE.getSLIMY(),playerEntity.getEquippedStack(EquipmentSlot.FEET)) > 0){
-            if (entity.bypassesLandingEffects()){
-                instance.onEntityLand(world,entity);
-            } else {
-                bounce(entity);
-            }
-
-        } else {
-            instance.onEntityLand(world,entity);
-        }
-
-    }*/
-
     @Shadow public float stepHeight;
 
     @Unique
