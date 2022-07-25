29be5c8b54eadec8f1f24c5ef673b00d01b5151b
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2022-07-25 14:07:28 -0400

    Update AltarEmiRecipe.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/compat/emi/AltarEmiRecipe.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/compat/emi/AltarEmiRecipe.kt
@@ -2,33 +2,26 @@ package me.fzzyhmstrs.amethyst_imbuement.compat.emi
 
 import me.fzzyhmstrs.amethyst_imbuement.AI
 import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterBlock
-import me.fzzyhmstrs.amethyst_imbuement.util.ImbuingRecipe
+import me.fzzyhmstrs.amethyst_imbuement.util.AltarRecipe
 import net.minecraft.util.Identifier
 import dev.emi.emi.api.recipe.*
 import dev.emi.emi.api.stack.EmiStack
 import dev.emi.emi.api.render.EmiTexture
 import dev.emi.emi.api.EmiPlugin
 
-class ImbuingEmiRecipe(recipe: ImbuingRecipe): EmiRecipe{
+class ImbuingEmiRecipe(recipe: AltarRecipe): EmiRecipe{
     
     private val id: Identifier
-    private val inputs: List<EmiIngredient>
-    private val outputs: List<EmiStack>
-    private val isEnchantingType: Boolean
+    private val base: EmiIngredient
+    private val addition: EmiIngredient
+    private val output: EmiStack
 
     init{
         id = recipe.id
-        inputs = initInputs(recipe)
-        outputs = initOutputs(recipe)
+        base = EmiIngredient.of(recipe.base)
+        addition = EmiIngredient.of(recipe.addition)
+	result = EmiStack.of(recipe.result)
     
-    }
-    
-    private fun initInputs(recipe: ImbuingRecipe): List<EmiIngredient>{
-        TODO()
-    }
-
-    private fun initoutputs(recipe: ImbuingRecipe): List<EmiStack>{
-        TODO()
     }
     
     override fun getCategory(): EmiRecipeCategory{
@@ -40,11 +33,11 @@ class ImbuingEmiRecipe(recipe: ImbuingRecipe): EmiRecipe{
     }
     
     override fun getInputs(): List<EmiIngredient>{
-        return inputs
+        return listOf(base,addition)
     }
     
     override fun getOutputs(): List<EmiStack>{
-        return outputs
+        return output
     }
     
     override fun getDisplayWidth(): Int{
@@ -58,9 +51,9 @@ class ImbuingEmiRecipe(recipe: ImbuingRecipe): EmiRecipe{
     override fun addWidgets(widgets: WidgetHolder){
         widgets.addTexture(EmiTexture.PLUS, 27, 3);
 		    widgets.addTexture(EmiTexture.EMPTY_ARROW, 75, 1);
-		    widgets.addSlot(inputs[0], 0, 0);
-		    widgets.addSlot(inputs[1], 49, 0);
-		    widgets.addSlot(outputs[0], 107, 0).recipeContext(this);
+		    widgets.addSlot(base, 0, 0);
+		    widgets.addSlot(addition, 49, 0);
+		    widgets.addSlot(output, 107, 0).recipeContext(this);
     }
 
 }
