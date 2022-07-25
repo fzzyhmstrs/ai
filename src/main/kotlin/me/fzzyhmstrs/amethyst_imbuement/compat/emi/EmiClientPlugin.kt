f34da06ba796b55624a2e7b769951a4af68cf327
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2022-07-25 10:25:40 -0400

    Update EmiClientPlugin.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/compat/emi/EmiClientPlugin.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/compat/emi/EmiClientPlugin.kt
@@ -2,6 +2,7 @@ package me.fzzyhmstrs.amethyst_imbuement.compat.emi
 
 import me.fzzyhmstrs.amethyst_imbuement.AI
 import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterBlock
+import me.fzzyhmstrs.amethyst_imbuement.util.ImbuingRecipe
 import net.minecraft.util.Identifier
 import dev.emi.emi.api.recipe.EmiRecipeCategory
 import dev.emi.emi.api.stack.EmiStack
@@ -16,6 +17,12 @@ class EmiClientPlugin: EmiPlugin{
     override fun register(registry: EmiRegistry){
         registry.addCategory(IMBUING_CATEGORY)
         registry.addWorkstation(IMBUING_CATEGORY, IMBUING_WORKSTATION)
+        
+        val manager = registry.getRecipeManager()
+        
+        for (recipe in manager.listAllOfType(ImbuingRecipe.Type)) {
+            registry.addRecipe(ImbuingEmiRecipe(recipe));
+        }
     }
     
 }
