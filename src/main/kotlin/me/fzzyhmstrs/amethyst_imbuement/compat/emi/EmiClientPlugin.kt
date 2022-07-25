88eb3e96be8ad6f31df0078a5489fb7d8476f432
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2022-07-25 13:16:42 -0400

    Update EmiClientPlugin.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/compat/emi/EmiClientPlugin.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/compat/emi/EmiClientPlugin.kt
@@ -10,9 +10,11 @@ import dev.emi.emi.api.render.EmiTexture
 import dev.emi.emi.api.EmiPlugin
 
 class EmiClientPlugin: EmiPlugin{
-    val SPRITE_SHEET = Identifier(AI.MOD_ID,"textures/gui/imbuing_textures.png")
+    val SPRITE_SHEET = Identifier(AI.MOD_ID,"textures/gui/emi_recipe_textures.png")
     val IMBUING_WORKSTATION = EmiStack.of(RegisterBlock.IMBUING_TABLE.asItem())
     val IMBUING_CATEGORY = EmiRecipeCategory(IMBUING_WORKSTATION, EmiTexture(SPRITE_SHEET, 0, 0, 16, 16))
+    val ALTAR_WORKSTATION = EmiStack.of(RegisterBlock.CRYSTAL_ALTAR.asItem())
+    val ALTAR_CATEGORY = EmiRecipeCategory(ALTAR_WORKSTATION, EmiTexture(SPRITE_SHEET, 0, 16, 16, 16))
     
     override fun register(registry: EmiRegistry){
         registry.addCategory(IMBUING_CATEGORY)
@@ -22,6 +24,15 @@ class EmiClientPlugin: EmiPlugin{
         
         for (recipe in manager.listAllOfType(ImbuingRecipe.Type)) {
             registry.addRecipe(ImbuingEmiRecipe(recipe));
+        }
+        
+        registry.addCategory(ALTAR_CATEGORY)
+        registry.addWorkstation(ALTAR_CATEGORY, ALTAR_WORKSTATION)
+        
+        val manager = registry.getRecipeManager()
+        
+        for (recipe in manager.listAllOfType(AltarRecipe.Type)) {
+            registry.addRecipe(AltarEmiRecipe(recipe));
         }
     }
     
