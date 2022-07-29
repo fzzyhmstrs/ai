dd40686afcfdd50c092ef623625872585c37d3d0
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-07-28 20:26:19 -0400

    re-introduce EMI entrypoint and initial completion of recipe method
    (cherry picked from commit 8fff3704af889d668953fa52088a136f16b172fa)

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/compat/emi/ImbuingEmiRecipe.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/compat/emi/ImbuingEmiRecipe.kt
@@ -10,6 +10,20 @@ import dev.emi.emi.api.render.EmiTexture
 import dev.emi.emi.api.EmiPlugin
 import dev.emi.emi.api.stack.EmiIngredient
 import dev.emi.emi.api.widget.WidgetHolder
+import me.fzzyhmstrs.amethyst_core.item_util.AbstractAugmentBookItem
+import me.fzzyhmstrs.amethyst_core.modifier_util.AugmentModifier
+import me.fzzyhmstrs.amethyst_core.modifier_util.ModifierHelper
+import me.fzzyhmstrs.amethyst_core.registry.ModifierRegistry
+import me.fzzyhmstrs.amethyst_core.scepter_util.augments.ScepterAugment
+import me.fzzyhmstrs.amethyst_core.trinket_util.base_augments.BaseAugment
+import me.fzzyhmstrs.amethyst_imbuement.item.BookOfLoreItem
+import me.fzzyhmstrs.amethyst_imbuement.item.BookOfMythosItem
+import me.shedaniel.rei.api.common.entry.EntryIngredient
+import me.shedaniel.rei.api.common.util.EntryStacks
+import net.minecraft.item.ItemStack
+import net.minecraft.item.Items
+import net.minecraft.recipe.Ingredient
+import net.minecraft.util.registry.Registry
 
 class ImbuingEmiRecipe(recipe: ImbuingRecipe): EmiRecipe{
     
@@ -27,11 +41,76 @@ class ImbuingEmiRecipe(recipe: ImbuingRecipe): EmiRecipe{
     }
     
     private fun initInputs(recipe: ImbuingRecipe): List<EmiIngredient>{
-        TODO()
+        val list: MutableList<EmiIngredient> = mutableListOf()
+        val inputs = recipe.getInputs()
+        for (i in inputs.indices){
+            val input = inputs[i]
+            if (recipe.getAugment() != "" && i == 6){
+                val identifier = Identifier(recipe.getAugment())
+                val enchant = Registry.ENCHANTMENT.get(identifier)
+                val modifier = ModifierRegistry.getByType<AugmentModifier>(identifier)
+                if (enchant != null){
+                    when (enchant) {
+                        is BaseAugment -> {
+                            val stacks = enchant.acceptableItemStacks().toTypedArray()
+                            val ingredient = Ingredient.ofStacks(*stacks)
+                            list.add(EmiIngredient.of(ingredient))
+                        }
+                        is ScepterAugment -> {
+                            val stacks = enchant.acceptableItemStacks().toTypedArray()
+                            val ingredient = Ingredient.ofStacks(*stacks)
+                            list.add(EmiIngredient.of(ingredient))
+                        }
+                        else -> {
+                            val enchantItemList: MutableList<ItemStack> = mutableListOf()
+                            for (item in Registry.ITEM.iterator()){
+                                val stack = ItemStack(item)
+                                if (enchant.isAcceptableItem(stack)){
+                                    enchantItemList.add(stack)
+                                }
+                            }
+                            val ingredient = Ingredient.ofStacks(*enchantItemList.toTypedArray())
+                            list.add(EmiIngredient.of(ingredient))
+                        }
+                    }
+                } else if(modifier != null) {
+                    val stacks = modifier.acceptableItemStacks().toTypedArray()
+                    val ingredient = Ingredient.ofStacks(*stacks)
+                    list.add(EmiIngredient.of(ingredient))
+                } else {
+                    list.add(EmiIngredient.of(Ingredient.EMPTY))
+                }
+                continue
+            }
+            list.add(EmiIngredient.of(input))
+        }
+        return list
     }
 
     private fun initOutputs(recipe: ImbuingRecipe): List<EmiStack>{
-        TODO()
+        val list: MutableList<EmiStack> = mutableListOf()
+        if (recipe.getAugment() == "") {
+            list.add(EmiStack.of(recipe.output))
+        } else {
+            val identifier = Identifier(recipe.getAugment())
+            val enchant = Registry.ENCHANTMENT.get(identifier)
+            val modifier = ModifierRegistry.getByType<AugmentModifier>(identifier)
+            val stack: ItemStack
+            if (enchant != null){
+                stack = ItemStack(Items.ENCHANTED_BOOK,1)
+                stack.addEnchantment(enchant,1)
+                list.add(EmiStack.of(stack))
+            } else if (modifier != null){
+                stack = modifier.acceptableItemStacks().first()
+                val moddedStack = stack.copy()
+                ModifierHelper.addModifierForREI(modifier.modifierId, moddedStack)
+                list.add(EmiStack.of(stack))
+            } else {
+                stack = ItemStack(Items.BOOK, 1)
+                list.add(EmiStack.of(stack))
+            }
+        }
+        return list
     }
     
     override fun getCategory(): EmiRecipeCategory{
