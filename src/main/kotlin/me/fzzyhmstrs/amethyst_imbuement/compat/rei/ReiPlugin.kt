be41c6d4c9f29d5a8f21698f027ea1a657750df4
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-07-30 17:52:00 -0400

    implement system for showing block recipes directly from EMI or REI.

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/compat/rei/ReiPlugin.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/compat/rei/ReiPlugin.kt
@@ -2,11 +2,20 @@
 
 package me.fzzyhmstrs.amethyst_imbuement.compat.rei
 
+import me.fzzyhmstrs.amethyst_imbuement.screen.ImbuingTableScreen
 import me.fzzyhmstrs.amethyst_imbuement.util.AltarRecipe
 import me.fzzyhmstrs.amethyst_imbuement.util.ImbuingRecipe
+import me.shedaniel.math.Rectangle
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin
 import me.shedaniel.rei.api.client.registry.category.CategoryRegistry
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry
+import me.shedaniel.rei.api.client.registry.screen.ScreenRegistry
+import me.shedaniel.rei.api.client.registry.screen.SimpleClickArea
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider
+import net.minecraft.data.server.ItemTagProvider
+import net.minecraft.tag.BlockTags
+import net.minecraft.tag.ItemTags
+import java.util.function.Supplier
 
 object ReiPlugin: REIClientPlugin {
 
@@ -26,4 +35,8 @@ object ReiPlugin: REIClientPlugin {
         registry.registerRecipeFiller(AltarRecipe::class.java,AltarRecipe.Type) { recipe -> CrystalAltarDisplay(recipe)}
     }
 
+    override fun registerScreens(registry: ScreenRegistry) {
+        val rectangle = Rectangle(6,91,20,18)
+        registry.registerContainerClickArea(rectangle,ImbuingTableScreen::class.java, IMBUING_TABLE_CATEGORY.categoryIdentifier)
+    }
 }
