ae6f5186433af677f955e5ef8b15d8fa80b2fd7f
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-08-05 15:22:57 -0400

    tweaks to get the soul missile particles working
    (cherry picked from commit 3e5842428bf292af033cbd852073ba9770ea2908)

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/entity/SoulMissileEntity.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/entity/SoulMissileEntity.kt
@@ -1,23 +1,53 @@
 package me.fzzyhmstrs.amethyst_imbuement.entity
 
 import me.fzzyhmstrs.amethyst_core.entity_util.MissileEntity
-import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterEntity
-import me.fzzyhmstrs.amethyst_core.modifier_util.AugmentEffect
 import me.fzzyhmstrs.amethyst_core.modifier_util.AugmentConsumer
+import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterEntity
 import net.minecraft.entity.EntityType
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.damage.DamageSource
-import net.minecraft.entity.projectile.SmallFireballEntity
 import net.minecraft.particle.ParticleEffect
 import net.minecraft.particle.ParticleTypes
+import net.minecraft.sound.SoundCategory
+import net.minecraft.sound.SoundEvents
+import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.util.hit.EntityHitResult
-import net.minecraft.util.math.MathHelper
 import net.minecraft.world.World
 
 class SoulMissileEntity: MissileEntity {
 
     constructor(entityType: EntityType<SoulMissileEntity>, world: World): super(entityType, world)
-    constructor(world: World,owner: LivingEntity, pierce: Boolean) : super(world, owner, pierce){
+
+    constructor(world: World, owner: LivingEntity) : super(RegisterEntity.SOUL_MISSILE_ENTITY, world){
+        this.owner = owner
+        this.setPosition(
+            owner.x,
+            owner.eyeY - 0.4,
+            owner.z
+        )
+        this.setRotation(owner.yaw, owner.pitch)
+    }
+
+    override fun onEntityHit(entityHitResult: EntityHitResult) {
+        if (world.isClient) {
+            return
+        }
+        val entity = owner
+        if (entity is LivingEntity) {
+            val entity2 = entityHitResult.entity
+            val bl: Boolean = entity2.damage(
+                    DamageSource.magic(this, entity).setProjectile(),
+                    entityEffects.damage(0)
+                )
+
+            if (bl) {
+                applyDamageEffects(entity, entity2)
+                if (entity2 is LivingEntity) {
+                    entityEffects.accept(entity2, AugmentConsumer.Type.HARMFUL)
+                }
+            }
+        }
+        discard()
     }
 
     override fun getParticleType(): ParticleEffect {
--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/scepter/SoulMissileAugment.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/scepter/SoulMissileAugment.kt
@@ -1,6 +1,5 @@
 package me.fzzyhmstrs.amethyst_imbuement.scepter
 
-import me.fzzyhmstrs.amethyst_core.entity_util.MissileEntity
 import me.fzzyhmstrs.amethyst_core.modifier_util.AugmentEffect
 import me.fzzyhmstrs.amethyst_core.scepter_util.LoreTier
 import me.fzzyhmstrs.amethyst_core.scepter_util.SpellType
@@ -23,7 +22,7 @@ class SoulMissileAugment(tier: Int, maxLvl: Int, vararg slot: EquipmentSlot): Su
         get() = super.baseEffect.withDamage(4.0F,0.0F,0.0F)
 
     override fun entityClass(world: World, user: LivingEntity, level: Int, effects: AugmentEffect): ProjectileEntity {
-        val me = SoulMissileEntity(world, user, true)
+        val me = SoulMissileEntity(world, user)
         me.setVelocity(user,user.pitch,user.yaw,0.0f,
             2.0f,
             0.1f)
