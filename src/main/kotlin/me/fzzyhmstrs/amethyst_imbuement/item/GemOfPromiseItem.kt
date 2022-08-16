73f9677912246b42a1cd76e482f4e2895bb34be5
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2022-08-16 11:59:27 -0400

    Update GemOfPromiseItem.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/GemOfPromiseItem.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/GemOfPromiseItem.kt
@@ -17,18 +17,44 @@ class GemOfPromiseItem(settings: Settings): Item(settings) {
         val record = entity.damageTracker.mostRecentDamage
     }
 
-    fun sparkingGemCheck(stack: ItemStack, world: World, playerEntity: PlayerEntity, record: DamageRecord){
-
-    }
-
-    fun blazingGemCheck(stack: ItemStack, world: World, playerEntity: PlayerEntity, record: DamageRecord){
-
-    }
-
 
     companion object{
-        fun incrementKillCount(stack: ItemStack, inventory: PlayerInventory){
+        
+        private val KILL_TARGET = 30
+        
+        fun healersGemCheck(stack: ItemStack, player: PlayerEntity){
+        
+        }
+        
+        fun brutalGemCheck(stack: ItemStack, player: PlayerEntity){
+        
+        }
+        
+        fun inquisitiveGemCheck(stack: ItemStack, player: PlayerEntity){
+        
+        }
+        
+        fun sparkingGemCheck(stack: ItemStack, world: World, playerEntity: PlayerEntity, record: DamageRecord){
 
+        }
+
+        fun blazingGemCheck(stack: ItemStack, world: World, playerEntity: PlayerEntity, record: DamageRecord){
+
+        }
+        
+        fun incrementKillCount(stack: ItemStack, inventory: PlayerInventory){
+            val nbt = stack.orCreateNbt
+            var kills: Int = 0
+            if (nbt.contains("kill_count")){
+                kills = Nbt.getIntNbt("kill_count",nbt)
+            }
+            val newCount = kills + 1
+            if (newCount >= KILL_TARGET){
+                stack.decrement(1)
+                TODO()
+            } else {
+                Nbt.writeIntNbt("kill_count", newCount,nbt)
+            }
         }
     }
 
