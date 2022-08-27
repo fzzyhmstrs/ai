62d2f10c4d68ad6b40e1e56251604388ce7618fb
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-08-27 14:17:40 -0400

    got modifiers working finally
    (cherry picked from commit d1d4a199e3ea851b8bf60bd771f76bbed19f0370)

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/amethyst_imbuement.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/amethyst_imbuement.kt
@@ -30,6 +30,7 @@ object AI: ModInitializer {
         RegisterStatus.registerAll()
         RegisterVillager.registerAll()
         RegisterRecipe.registerAll()
+        RegisterModifier.registerAll()
         RegisterKeybindServer.registerServer()
         AltarOfExperienceScreenHandler.registerServer()
     }
