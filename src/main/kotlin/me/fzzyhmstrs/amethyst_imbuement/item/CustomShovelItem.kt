24962610c08833c5f30c0155e093421c7f077ce5
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-08-28 21:28:32 -0400

    finished out the custom[tool]item collection, and all are flavor items now
    (cherry picked from commit 419bca4a4f018bac2462aa6cfb43f58352c65ccf)

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/CustomShovelItem.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/CustomShovelItem.kt
@@ -1,17 +1,18 @@
 package me.fzzyhmstrs.amethyst_imbuement.item
 
 import me.fzzyhmstrs.amethyst_core.item_util.interfaces.Flavorful
-import net.minecraft.item.HoeItem
+import net.minecraft.item.AxeItem
+import net.minecraft.item.ShovelItem
 import net.minecraft.item.ToolMaterial
 
-class CustomHoeItem(material: ToolMaterial, attackDamage: Int, attackSpeed: Float, settings: Settings):
-    HoeItem(material, attackDamage, attackSpeed, settings), Flavorful<CustomHoeItem> {
+class CustomShovelItem(material: ToolMaterial, attackDamage: Float, attackSpeed: Float, settings: Settings):
+    ShovelItem(material, attackDamage, attackSpeed, settings), Flavorful<CustomShovelItem> {
     // just to make this public
     override var flavor: String = ""
     override var flavorDesc: String = ""
     override var glint: Boolean = false
 
-    override fun getFlavorItem(): CustomHoeItem {
+    override fun getFlavorItem(): CustomShovelItem {
         return this
     }
 }
