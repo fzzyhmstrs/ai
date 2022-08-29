d7c9af0c7b2206b228c7fa0c66679cd64a2b1111
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-08-28 21:29:58 -0400

    centralized registerServer/registerClient calls into one place

---
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/registry/RegisterNetworking.kt
@@ -0,0 +1,18 @@
+package me.fzzyhmstrs.amethyst_imbuement.registry
+
+import me.fzzyhmstrs.amethyst_imbuement.scepter.ResonateAugment
+import me.fzzyhmstrs.amethyst_imbuement.scepter.SmitingBlowAugment
+import me.fzzyhmstrs.amethyst_imbuement.screen.ImbuingTableScreenHandler
+
+object RegisterNetworking {
+
+    fun registerServer(){
+        RegisterKeybindServer.registerServer()
+        ImbuingTableScreenHandler.registerServer()
+    }
+    fun registerClient(){
+        ImbuingTableScreenHandler.registerClient()
+        ResonateAugment.registerClient()
+        SmitingBlowAugment.registerClient()
+    }
+}
