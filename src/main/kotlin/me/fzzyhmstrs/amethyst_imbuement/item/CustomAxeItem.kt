419bca4a4f018bac2462aa6cfb43f58352c65ccf
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-08-28 21:28:32 -0400

    finished out the custom[tool]item collection, and all are flavor items now

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/CustomAxeItem.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/CustomAxeItem.kt
@@ -1,8 +1,17 @@
 package me.fzzyhmstrs.amethyst_imbuement.item
 
+import me.fzzyhmstrs.amethyst_core.item_util.interfaces.Flavorful
 import net.minecraft.item.AxeItem
 import net.minecraft.item.ToolMaterial
 
-class CustomAxeItem(material: ToolMaterial, attackDamage: Float, attackSpeed: Float, settings: Settings): AxeItem(material, attackDamage, attackSpeed, settings) {
+class CustomAxeItem(material: ToolMaterial, attackDamage: Float, attackSpeed: Float, settings: Settings):
+    AxeItem(material, attackDamage, attackSpeed, settings), Flavorful<CustomAxeItem> {
     // just to make this public
+    override var flavor: String = ""
+    override var flavorDesc: String = ""
+    override var glint: Boolean = false
+
+    override fun getFlavorItem(): CustomAxeItem {
+        return this
+    }
 }
