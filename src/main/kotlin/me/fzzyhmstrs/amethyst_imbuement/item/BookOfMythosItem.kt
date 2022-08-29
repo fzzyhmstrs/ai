114d1baa8297818e73f03acc8a25a6b38ec4275e
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-08-28 21:27:27 -0400

    knowledge books now better at type checking if you hold an appropriate scepter type. added tags for that.

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/BookOfMythosItem.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/BookOfMythosItem.kt
@@ -5,6 +5,9 @@ import me.fzzyhmstrs.amethyst_core.nbt_util.Nbt
 import me.fzzyhmstrs.amethyst_core.nbt_util.NbtKeys
 import me.fzzyhmstrs.amethyst_core.scepter_util.LoreTier
 import me.fzzyhmstrs.amethyst_core.scepter_util.ScepterHelper
+import me.fzzyhmstrs.amethyst_core.scepter_util.SpellType
+import me.fzzyhmstrs.amethyst_core.scepter_util.augments.AugmentHelper
+import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterTag
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
 import net.minecraft.server.world.ServerWorld
@@ -16,4 +19,39 @@ import net.minecraft.world.World
 
 class BookOfMythosItem(settings: Settings) : AbstractAugmentBookItem(settings) {
     override val loreTier: LoreTier = LoreTier.HIGH_TIER
+
+    override fun getRandomBookAugment(list: List<String>, user: PlayerEntity, hand: Hand): String {
+        val stack = if (hand == Hand.MAIN_HAND){
+            user.offHandStack
+        } else {
+            user.mainHandStack
+        }
+        if (stack.isIn(RegisterTag.ALL_FURY_SCEPTERS_TAG)){
+            for (i in 1..2){
+                val aug = super.getRandomBookAugment(list, user, hand)
+                val type = AugmentHelper.getAugmentType(aug)
+                if (type == SpellType.FURY){
+                    return aug
+                }
+            }
+        } else if (stack.isIn(RegisterTag.ALL_WIT_SCEPTERS_TAG)){
+            for (i in 1..2){
+                val aug = super.getRandomBookAugment(list, user, hand)
+                val type = AugmentHelper.getAugmentType(aug)
+                if (type == SpellType.WIT){
+                    return aug
+                }
+            }
+        } else if (stack.isIn(RegisterTag.ALL_GRACE_SCEPTERS_TAG)){
+            for (i in 1..2){
+                val aug = super.getRandomBookAugment(list, user, hand)
+                val type = AugmentHelper.getAugmentType(aug)
+                if (type == SpellType.GRACE){
+                    return aug
+                }
+            }
+        }
+
+        return super.getRandomBookAugment(list, user, hand)
+    }
 }
--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/registry/RegisterTag.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/registry/RegisterTag.kt
@@ -25,10 +25,14 @@ object RegisterTag {
     val RESONANCE_SCEPTERS_TAG: TagKey<Item> = TagKey.of(Registry.ITEM_KEY,Identifier(AI.MOD_ID,"resonance_scepters"))
     val SOJOURN_SCEPTERS_TAG: TagKey<Item> = TagKey.of(Registry.ITEM_KEY,Identifier(AI.MOD_ID,"sojourn_scepters"))
     val AEGIS_SCEPTERS_TAG: TagKey<Item> = TagKey.of(Registry.ITEM_KEY,Identifier(AI.MOD_ID,"aegis_scepters"))
-    val REDEMPTION_SCEPTERS_TAG: TagKey<Item> = TagKey.of(Registry.ITEM_KEY,Identifier(AI.MOD_ID,"REDEMPTION_scepters"))
+    val REDEMPTION_SCEPTERS_TAG: TagKey<Item> = TagKey.of(Registry.ITEM_KEY,Identifier(AI.MOD_ID,"redemption_scepters"))
     val FURY_SCEPTERS_TAG: TagKey<Item> = TagKey.of(Registry.ITEM_KEY,Identifier(AI.MOD_ID,"fury_scepters"))
     val WIT_SCEPTERS_TAG: TagKey<Item> = TagKey.of(Registry.ITEM_KEY,Identifier(AI.MOD_ID,"wit_scepters"))
     val GRACE_SCEPTERS_TAG: TagKey<Item> = TagKey.of(Registry.ITEM_KEY,Identifier(AI.MOD_ID,"grace_scepters"))
+
+    val ALL_FURY_SCEPTERS_TAG: TagKey<Item> = TagKey.of(Registry.ITEM_KEY,Identifier(AI.MOD_ID,"all_fury_scepters"))
+    val ALL_WIT_SCEPTERS_TAG: TagKey<Item> = TagKey.of(Registry.ITEM_KEY,Identifier(AI.MOD_ID,"all_wit_scepters"))
+    val ALL_GRACE_SCEPTERS_TAG: TagKey<Item> = TagKey.of(Registry.ITEM_KEY,Identifier(AI.MOD_ID,"all_grace_scepters"))
     
 
 }
