f7b80f7b0047e76615449f436bc28442fe3278ed
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-10-14 17:28:19 -0400

    update ClientPlayNetworkHandlerMixin to look at the whole player inv, and also to not use redirect
    (cherry picked from commit 5f69b4962005a3a3e57b4f37c153145c70c55588)

--- src/main/java/me/fzzyhmstrs/amethyst_imbuement/mixins/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/me/fzzyhmstrs/amethyst_imbuement/mixins/ClientPlayNetworkHandlerMixin.java
@@ -4,22 +4,34 @@ import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterEnchantment;
 import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterItem;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public class ClientPlayNetworkHandlerMixin {
 
-    @Redirect(method = "getActiveTotemOfUndying", at = @At(value = "INVOKE", target = "net/minecraft/item/ItemStack.isOf (Lnet/minecraft/item/Item;)Z"))
-    private static boolean checkForTotemOfAmethyst(ItemStack instance, Item item){
-        if (instance.isOf(Items.TOTEM_OF_UNDYING)) return true;
-        boolean bl1 = instance.isOf(RegisterItem.INSTANCE.getTOTEM_OF_AMETHYST());
-        bl1 = bl1 && EnchantmentHelper.getLevel(RegisterEnchantment.INSTANCE.getUNDYING(), instance) > 0;
-        return bl1;
+    @Inject(method = "getActiveTotemOfUndying", at = @At(value = "TAIL"), cancellable = true)
+    private static void checkForTotemOfAmethyst(PlayerEntity player, CallbackInfoReturnable<ItemStack> cir){
+        player.getInventory().main.forEach(stack->{
+            if (stack.isOf(RegisterItem.INSTANCE.getTOTEM_OF_AMETHYST())){
+                if (EnchantmentHelper.getLevel(RegisterEnchantment.INSTANCE.getUNDYING(), stack) > 0){
+                    cir.setReturnValue(stack);
+                }
+            }
+        });
+        player.getInventory().offHand.forEach(stack-> {
+            if (stack.isOf(RegisterItem.INSTANCE.getTOTEM_OF_AMETHYST())) {
+                if (EnchantmentHelper.getLevel(RegisterEnchantment.INSTANCE.getUNDYING(), stack) > 0) {
+                    cir.setReturnValue(stack);
+                }
+            }
+        });
     }
-
 }
