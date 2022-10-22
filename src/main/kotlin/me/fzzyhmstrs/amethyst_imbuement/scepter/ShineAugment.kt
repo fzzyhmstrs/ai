9df06e8b832fd43ebeabab3f6ed7fbf7f995abed
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-10-21 20:46:31 -0400

    fix LifestealEnchantment.kt and update ShineAugment.kt for new placeitemaugment impl

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/scepter/ShineAugment.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/scepter/ShineAugment.kt
@@ -1,26 +1,17 @@
 package me.fzzyhmstrs.amethyst_imbuement.scepter
 
-import me.fzzyhmstrs.amethyst_core.modifier_util.AugmentEffect
-import me.fzzyhmstrs.amethyst_core.raycaster_util.RaycasterUtil
 import me.fzzyhmstrs.amethyst_core.scepter_util.LoreTier
 import me.fzzyhmstrs.amethyst_core.scepter_util.SpellType
 import me.fzzyhmstrs.amethyst_core.scepter_util.augments.AugmentDatapoint
 import me.fzzyhmstrs.amethyst_core.scepter_util.augments.BuilderAugment
 import me.fzzyhmstrs.amethyst_core.scepter_util.augments.PlaceItemAugment
 import net.minecraft.entity.EquipmentSlot
-import net.minecraft.entity.LivingEntity
 import net.minecraft.item.Item
 import net.minecraft.item.Items
 import net.minecraft.sound.SoundEvent
 import net.minecraft.sound.SoundEvents
-import net.minecraft.util.Hand
-import net.minecraft.world.World
 
 class ShineAugment(tier: Int, maxLvl: Int, item: Item, vararg slot: EquipmentSlot): PlaceItemAugment(tier, maxLvl, item, *slot), BuilderAugment {
-
-    override fun applyTasks(world: World, user: LivingEntity, hand: Hand, level: Int, effects: AugmentEffect): Boolean {
-        return RaycasterUtil.raycastBlock(entity = user) != null
-    }
 
     override fun augmentStat(imbueLevel: Int): AugmentDatapoint {
         return AugmentDatapoint(SpellType.WIT,5,2,1,imbueLevel,LoreTier.NO_TIER, Items.TORCH)
