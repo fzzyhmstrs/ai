12cebc50006403851fd79aec0cd63757a97a2ba2
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-11-01 21:03:20 -0400

    deleting remappedSrc

--- remappedSrc/me/fzzyhmstrs/amethyst_imbuement/mixins/CrossbowUserMixin.java
+++
@@ -1,9 +0,0 @@
-package me.fzzyhmstrs.amethyst_imbuement.mixins;
-
-
-import net.minecraft.entity.CrossbowUser;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(CrossbowUser.class)
-public class CrossbowUserMixin {
-}
