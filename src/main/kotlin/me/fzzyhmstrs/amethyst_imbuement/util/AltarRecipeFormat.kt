9c3c0ca966b0c0f4ea565d8d61d6617aa34cf098
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-11-03 22:36:42 -0400

    Refactored recipe classes and added a new util for working with them. Moved some recipe stuff out of the CompatHelper into the recipe and util

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/util/AltarRecipeFormat.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/util/AltarRecipeFormat.kt
@@ -3,10 +3,9 @@ package me.fzzyhmstrs.amethyst_imbuement.util
 import com.google.gson.JsonObject
 
 class AltarRecipeFormat {
+
     var base: JsonObject? = null
     var addition: JsonObject? = null
     var result: JsonObject? = null
-
-
 
 }
--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/util/ImbuingRecipeFormat.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/util/ImbuingRecipeFormat.kt
@@ -3,6 +3,7 @@ package me.fzzyhmstrs.amethyst_imbuement.util
 import com.google.gson.JsonObject
 
 class ImbuingRecipeFormat {
+
     var type: String = "amethyst_imbuement:imbuing"
     var imbueA: JsonObject? = null
     var imbueB: JsonObject? = null
@@ -17,12 +18,12 @@ class ImbuingRecipeFormat {
     var craftG: JsonObject? = null
     var craftH: JsonObject? = null
     var craftI: JsonObject? = null
-    var title: String? = null
-    var cost: Int? = null
-    var augment: String? = null
-    var resultA: String? = null
-    var countA: Int? = null
-    var transferEnchant: Boolean? = null
+    var title: String = ""
+    var cost: Int = 0
+    var augment: String = ""
+    var resultA: String = ""
+    var countA: Int = 0
+    var transferEnchant: Boolean = false
 
 
 }
