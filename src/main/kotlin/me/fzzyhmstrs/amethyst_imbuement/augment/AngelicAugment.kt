c5af6b7d3ae8f2d511dcec40a499f144d7f3efe7
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-11-28 20:56:12 -0500

    Adding persistent effect to Angelic to check that the totem is still in the inventory. If not, revokes flying.
    (cherry picked from commit 2e94dd8825aa25e980ec1f0ff09438c20950e03b)

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/augment/AngelicAugment.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/augment/AngelicAugment.kt
@@ -5,12 +5,16 @@ import io.github.ladysnake.pal.Pal
 import io.github.ladysnake.pal.PlayerAbility
 import io.github.ladysnake.pal.VanillaAbilities
 import me.fzzyhmstrs.amethyst_core.coding_util.AcText
+import me.fzzyhmstrs.amethyst_core.coding_util.PerLvlI
+import me.fzzyhmstrs.amethyst_core.coding_util.PersistentEffectHelper
 import me.fzzyhmstrs.amethyst_core.nbt_util.NbtKeys
 import me.fzzyhmstrs.amethyst_imbuement.AI
 import me.fzzyhmstrs.amethyst_imbuement.augment.base_augments.ActiveAugment
 import me.fzzyhmstrs.amethyst_imbuement.item.TotemItem
 import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterEnchantment
 import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterItem
+import me.fzzyhmstrs.amethyst_imbuement.augment.base_augments.*
+import me.fzzyhmstrs.amethyst_imbuement.item.TotemItem
 import net.minecraft.entity.EquipmentSlot
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.player.PlayerEntity
@@ -20,11 +24,14 @@ import net.minecraft.util.Identifier
 
 class AngelicAugment(weight: Rarity, mxLvl: Int = 1, vararg slot: EquipmentSlot): ActiveAugment(weight,mxLvl,*slot) {
 
-    private val ability: PlayerAbility = VanillaAbilities.ALLOW_FLYING
-    private val abilitySource: AbilitySource = Pal.getAbilitySource(Identifier(AI.MOD_ID,"angelic"))
+    companion object {
+        private val ability: PlayerAbility = VanillaAbilities.ALLOW_FLYING
+        private val abilitySource: AbilitySource = Pal.getAbilitySource(Identifier(AI.MOD_ID, "angelic"))
+    }
 
     override fun activateEffect(user: LivingEntity, level: Int, stack: ItemStack) {
         user as PlayerEntity
+        AngelicPersistentEffect(user,stack)
         if (!user.abilities.creativeMode) {
             if (!abilitySource.grants(user,ability)){
                 abilitySource.grantTo(user,ability)
@@ -51,5 +58,29 @@ class AngelicAugment(weight: Rarity, mxLvl: Int = 1, vararg slot: EquipmentSlot)
             abilitySource.revokeFrom(user,ability)
         }
     }
+
+    private class AngelicPersistentEffect(user: PlayerEntity, stack: ItemStack): PersistentEffectHelper.PersistentEffect {
+
+        init{
+            val data = AngelicPersistentData(user, stack)
+            PersistentEffectHelper.setPersistentTickerNeed(this,21,21,data)
+        }
+
+        override val delay: PerLvlI = PerLvlI()
+
+        override fun persistentEffect(data: PersistentEffectHelper.PersistentEffectData) {
+            if (data is AngelicPersistentData){
+                if (!data.user.inventory.contains(data.stack)){
+                    abilitySource.revokeFrom(data.user, ability)
+                } else {
+                    if (!TotemItem.getActive(data.stack)){
+                        abilitySource.revokeFrom(data.user, ability)
+                    }
+                }
+            }
+        }
+    }
+
+    private class AngelicPersistentData(val user: PlayerEntity, val stack: ItemStack): PersistentEffectHelper.PersistentEffectData
 
 }
