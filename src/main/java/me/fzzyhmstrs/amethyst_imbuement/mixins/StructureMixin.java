326c834026731ea1d2a9e10d699abc0742a5f21e
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-12-29 16:15:43 -0500

    Fixup mixins for successful build

--- src/main/java/me/fzzyhmstrs/amethyst_imbuement/mixins/StructureMixin.java
+++ src/main/java/me/fzzyhmstrs/amethyst_imbuement/mixins/StructureMixin.java
@@ -4,14 +4,16 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import me.fzzyhmstrs.amethyst_imbuement.config.AiConfig;
 import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterBlock;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtHelper;
 import net.minecraft.nbt.NbtList;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryEntryLookup;
 import net.minecraft.structure.StructureTemplate;
-import net.minecraft.util.registry.Registry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -30,12 +32,12 @@ public class StructureMixin {
     }*/
 
     @WrapOperation(method = "loadPalettedBlockInfo", at = @At(value = "INVOKE", target = "net/minecraft/nbt/NbtList.getCompound (I)Lnet/minecraft/nbt/NbtCompound;"))
-    private NbtCompound amethyst_imbuement_checkForEnchantingTable(NbtList instance, int index, Operation<NbtCompound> operation){
+    private NbtCompound amethyst_imbuement_checkForEnchantingTable(NbtList instance, int index, Operation<NbtCompound> operation, RegistryEntryLookup<Block> blockLookup, NbtList palette, NbtList blocks){
         NbtCompound nbtCompound = operation.call(instance,index);
-        BlockState state = NbtHelper.toBlockState(nbtCompound);
+        BlockState state = NbtHelper.toBlockState(blockLookup,nbtCompound);
         if (state.isOf(Blocks.ENCHANTING_TABLE) && AiConfig.INSTANCE.getAltars().getImbuingTableReplaceEnchantingTable()){
             Item table = RegisterBlock.INSTANCE.getIMBUING_TABLE().asItem();
-            nbtCompound.putString("Name", Registry.ITEM.getId(table).toString());
+            nbtCompound.putString("Name", Registries.ITEM.getId(table).toString());
         }
         return nbtCompound;
     }
