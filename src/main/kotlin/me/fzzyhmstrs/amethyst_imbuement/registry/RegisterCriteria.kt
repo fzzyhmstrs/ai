2edd2d5eb5421780c1607a5f81321b93681fab4c
fzzyhmstrs <fzzyhmstrs@gmail.com>
2022-12-29 21:31:12 -0500

    fix up no-logic criteria

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/registry/RegisterCriteria.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/registry/RegisterCriteria.kt
@@ -1,29 +1,29 @@
 package me.fzzyhmstrs.amethyst_imbuement.registry
 
+import me.fzzyhmstrs.amethyst_core.scepter_util.CustomCriterion
 import me.fzzyhmstrs.amethyst_imbuement.AI
 import me.fzzyhmstrs.amethyst_imbuement.config.AiConfig
 import me.fzzyhmstrs.amethyst_imbuement.util.AugmentCriterion
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents
 import net.minecraft.advancement.criterion.Criteria
-import net.minecraft.advancement.criterion.TickCriterion
 import net.minecraft.util.Identifier
 
 object RegisterCriteria {
 
-    val GIVE_IF_CONFIG = TickCriterion(Identifier(AI.MOD_ID,"give_if_config"))
-    val DISENCHANT_USE = TickCriterion(Identifier(AI.MOD_ID,"disenchant_use"))
-    val DISENCHANTING_PILLARS = TickCriterion(Identifier(AI.MOD_ID,"disenchanting_pillars"))
-    val DISENCHANTING_MAX = TickCriterion(Identifier(AI.MOD_ID,"disenchanting_max"))
-    val ALTAR_USE = TickCriterion(Identifier(AI.MOD_ID,"altar_use"))
-    val CANDLELIT_ALTAR = TickCriterion(Identifier(AI.MOD_ID,"candlelit_altar"))
-    val CANDLELIT_MAX = TickCriterion(Identifier(AI.MOD_ID,"candlelit_max"))
-    val IMBUING_USE = TickCriterion(Identifier(AI.MOD_ID,"imbuing_use"))
-    val AMPED_UP = TickCriterion(Identifier(AI.MOD_ID,"amped_up"))
-    val MASTER_RESEARCHER = TickCriterion(Identifier(AI.MOD_ID,"master_researcher"))
-    val DEVOUT_CLERIC = TickCriterion(Identifier(AI.MOD_ID,"devout_cleric"))
+    val GIVE_IF_CONFIG = CustomCriterion(Identifier(AI.MOD_ID,"give_if_config"))
+    val DISENCHANT_USE = CustomCriterion(Identifier(AI.MOD_ID,"disenchant_use"))
+    val DISENCHANTING_PILLARS = CustomCriterion(Identifier(AI.MOD_ID,"disenchanting_pillars"))
+    val DISENCHANTING_MAX = CustomCriterion(Identifier(AI.MOD_ID,"disenchanting_max"))
+    val ALTAR_USE = CustomCriterion(Identifier(AI.MOD_ID,"altar_use"))
+    val CANDLELIT_ALTAR = CustomCriterion(Identifier(AI.MOD_ID,"candlelit_altar"))
+    val CANDLELIT_MAX = CustomCriterion(Identifier(AI.MOD_ID,"candlelit_max"))
+    val IMBUING_USE = CustomCriterion(Identifier(AI.MOD_ID,"imbuing_use"))
+    val AMPED_UP = CustomCriterion(Identifier(AI.MOD_ID,"amped_up"))
+    val MASTER_RESEARCHER = CustomCriterion(Identifier(AI.MOD_ID,"master_researcher"))
+    val DEVOUT_CLERIC = CustomCriterion(Identifier(AI.MOD_ID,"devout_cleric"))
     val AUGMENT_ITEM = AugmentCriterion(Identifier(AI.MOD_ID,"augment_item"))
-    val ENHANCE = TickCriterion(Identifier(AI.MOD_ID,"enhance"))
-    val IGNITE = TickCriterion(Identifier(AI.MOD_ID,"ignite"))
+    val ENHANCE = CustomCriterion(Identifier(AI.MOD_ID,"enhance"))
+    val IGNITE = CustomCriterion(Identifier(AI.MOD_ID,"ignite"))
 
     fun registerAll(){
         Criteria.register(GIVE_IF_CONFIG)
