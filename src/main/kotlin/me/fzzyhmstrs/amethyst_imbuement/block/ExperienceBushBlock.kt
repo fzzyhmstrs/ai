1923c3ba057ccb56adf70d8b31ad74189bffd644
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-02-03 18:58:06 -0500

    fix config option the grow method uses
    (cherry picked from commit 4c80a1191d8df9415ca016be5104eb4eb1aed92e)

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/block/ExperienceBushBlock.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/block/ExperienceBushBlock.kt
@@ -87,7 +87,7 @@ class ExperienceBushBlock(settings: Settings):SweetBerryBushBlock(settings) {
 
 
     override fun grow(world: ServerWorld, random: Random, pos: BlockPos, state: BlockState) {
-        if (random.nextFloat() < AiConfig.altars.experienceBushGrowChance) {
+        if (random.nextFloat() < AiConfig.altars.experienceBushBonemealChance) {
             val i = 3.coerceAtMost(state.get(AGE) + 1)
             world.setBlockState(pos, state.with(AGE, i) as BlockState, NOTIFY_LISTENERS)
         }
