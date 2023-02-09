4e6a50c0ca63c3b80dfb1068e84a2cae5b878ed2
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-02-09 16:47:05 -0500

    Add totem of amethyst durability config

--- changelog.md
+++ changelog.md
@@ -1,19 +1,10 @@
 ### Additions
-* Added config options for experience bush growth chance and bonemeal chance in the Altar_v3 config file
-* Added integration with Enchanting Table Descriptions; the imbuing table tooltips will now show the enchantment and augment descriptions in the same manner as Enchantment Descriptions.
-* Added alternate gem dust recipes for Citrine, Smoky Quartz, and Danburite
-* zh_cn localization added.
+* Re-added JEI plugin
+* Added reroll cost integration with Gearifiers
+* Added config option for Totem of Amethyst durability
 
 ### Changes
-* **Now powered by Fzzy Core and the refactored Amethyst Core. These libraries are part of a project to divide the previous Amethyst Core into more streamlined and focused parts. This does not change any functionality of AI, but it will now require downloading AC and FC as dependencies. I will no longer be embedding them.**
-* Mana potions now repair slightly more.
-* Mana potions throw like experience bottles if there is nothing to heal rather than having to drink them for XP.
-* Increased the drinking speed of mana potions.
-* Scepter modifiers have a new layout now, with descriptions (if advanced tooltips are active)
-* Spell types now utilize a tag system, opening the possibility for creators to change what spells are defined as what.
+* Totems of Amethyst will now fail to activate if they are at very low durability
 
 ### Fixes
-* Mana potions now repair items in the offhand and armor slots
-* Imbuing tables now drop their inventory when broken
-* EMI can now transfer augment recipes into the imbuing table properly.
-* Two hidden advancements now properly have loot tables.
+* Fixed scepters not properly upgrading modifiers when imbued to higher tiers.
--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/registry/RegisterItem.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/registry/RegisterItem.kt
@@ -4,6 +4,7 @@ package me.fzzyhmstrs.amethyst_imbuement.registry
 
 import me.fzzyhmstrs.amethyst_core.registry.ModifierRegistry
 import me.fzzyhmstrs.amethyst_imbuement.AI
+import me.fzzyhmstrs.amethyst_imbuement.config.AiConfig
 import me.fzzyhmstrs.amethyst_imbuement.item.*
 import me.fzzyhmstrs.amethyst_imbuement.item.AiItemSettings.AiItemGroup
 import me.fzzyhmstrs.amethyst_imbuement.tool.*
@@ -89,7 +90,7 @@ object RegisterItem {
     val IMBUED_RING = ImbuedJewelryItem(AiItemSettings().aiGroup(AiItemGroup.EQUIPMENT).maxCount(1)).withFlavorDefaultPath(Identifier(AI.MOD_ID,"imbued_ring")).withFlavorDescDefaultPath(Identifier(AI.MOD_ID,"imbued_ring")) .also{ regItem["imbued_ring"] = it}
     val IMBUED_HEADBAND = ImbuedJewelryItem(AiItemSettings().aiGroup(AiItemGroup.EQUIPMENT).maxCount(1)).withFlavorDefaultPath(Identifier(AI.MOD_ID,"imbued_headband")).withFlavorDescDefaultPath(Identifier(AI.MOD_ID,"imbued_headband")).also{ regItem["imbued_headband"] = it}
     val IMBUED_AMULET = ImbuedJewelryItem(AiItemSettings().aiGroup(AiItemGroup.EQUIPMENT).maxCount(1)).withFlavorDefaultPath(Identifier(AI.MOD_ID,"imbued_amulet")).withFlavorDescDefaultPath(Identifier(AI.MOD_ID,"imbued_amulet")).also{ regItem["imbued_amulet"] = it}
-    val TOTEM_OF_AMETHYST = TotemItem(AiItemSettings().aiGroup(AiItemGroup.EQUIPMENT).maxDamage(360).rarity(Rarity.UNCOMMON)).also{ regItem["totem_of_amethyst"] = it}
+    val TOTEM_OF_AMETHYST = TotemItem(AiItemSettings().aiGroup(AiItemGroup.EQUIPMENT).maxDamage(AiConfig.items.totemOfAmethystDurability).rarity(Rarity.UNCOMMON)).also{ regItem["totem_of_amethyst"] = it}
     val BOOK_OF_LORE = BookOfLoreItem(FabricItemSettings().maxCount(1)).withFlavorDefaultPath(Identifier(AI.MOD_ID,"book_of_lore")).withFlavorDescDefaultPath(Identifier(AI.MOD_ID,"book_of_lore")) .also{ regItem["book_of_lore"] = it}
     val BOOK_OF_MYTHOS = BookOfMythosItem(FabricItemSettings().maxCount(1).rarity(Rarity.RARE)).withFlavorDefaultPath(Identifier(AI.MOD_ID,"book_of_mythos")).withFlavorDescDefaultPath(Identifier(AI.MOD_ID,"book_of_mythos")).withGlint() .also{ regItem["book_of_mythos"] = it}
     val MANA_POTION = ManaPotionItem(FabricItemSettings().maxCount(16)).also{ regItem["mana_potion"] = it}
