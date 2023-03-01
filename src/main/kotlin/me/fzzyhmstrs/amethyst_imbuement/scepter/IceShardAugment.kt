397878e68fb95a382880872e9947c0b4ea0c7c02
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-03-01 12:20:32 -0500

    Create IceShardAugment.kt
    (cherry picked from commit 583a877ffda0b5ed14d988caa7580fad7c426082)

---
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/scepter/IceShardAugment.kt
@@ -0,0 +1,39 @@
+package me.fzzyhmstrs.amethyst_imbuement.scepter
+
+import me.fzzyhmstrs.amethyst_core.modifier_util.AugmentEffect
+import me.fzzyhmstrs.amethyst_core.scepter_util.LoreTier
+import me.fzzyhmstrs.amethyst_core.scepter_util.SpellType
+import me.fzzyhmstrs.amethyst_core.scepter_util.augments.AugmentDatapoint
+import me.fzzyhmstrs.amethyst_core.scepter_util.augments.SummonProjectileAugment
+import me.fzzyhmstrs.amethyst_imbuement.entity.FlameboltEntity
+import net.minecraft.entity.EquipmentSlot
+import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.projectile.ProjectileEntity
+import net.minecraft.item.Items
+import net.minecraft.sound.SoundEvent
+import net.minecraft.sound.SoundEvents
+import net.minecraft.world.World
+
+class IceShardAugment(tier: Int, maxLvl: Int, vararg slot: EquipmentSlot): SummonProjectileAugment(tier, maxLvl, *slot){
+
+    override val baseEffect: AugmentEffect
+        get() = super.baseEffect
+            .withDamage(3.75F,0.25F,0.0F)
+            .withDuration(180,20)
+            .withAmplifier(1)
+
+    override fun entityClass(world: World, user: LivingEntity, level: Int, effects: AugmentEffect): ProjectileEntity {
+        val speed = 4.5F
+        val div = 0.75F
+        return IceShardEntity.createIceShard(world, user, speed, div, effects, level)
+    }
+
+    override fun soundEvent(): SoundEvent {
+        return SoundEvents.ENTITY_BLAZE_SHOOT
+    }
+
+    override fun augmentStat(imbueLevel: Int): AugmentDatapoint {
+        return AugmentDatapoint(SpellType.FURY,10,4,10, imbueLevel, LoreTier.LOW_TIER, Items.BLUE_ICE)
+    }
+
+}
