2a6ccebf23cb643f5a3dea49cbfc59c23478c580
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-03-03 16:52:43 -0500

    update how resilience and steadfast apply their modifiers
    (cherry picked from commit b245475c859d36ce63f47e6ccd9541179b7b8182)

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/enchantment/SteadfastEnchantment.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/enchantment/SteadfastEnchantment.kt
@@ -4,9 +4,16 @@ import net.minecraft.enchantment.EnchantmentTarget
 import net.minecraft.entity.EquipmentSlot
 import net.minecraft.item.ArmorItem
 import net.minecraft.item.ItemStack
+import java.util.*
 
 class SteadfastEnchantment(weight: Rarity, vararg slot: EquipmentSlot): ConfigDisableEnchantment(weight, EnchantmentTarget.ARMOR,*slot) {
 
+    internal val uuids: EnumMap<EquipmentSlot, UUID> = EnumMap(mapOf(
+        EquipmentSlot.HEAD to UUID.fromString("797ca106-ba09-11ed-afa1-0242ac120002"),
+        EquipmentSlot.CHEST to UUID.fromString("797ca52a-ba09-11ed-afa1-0242ac120002"),
+        EquipmentSlot.LEGS to UUID.fromString("797ca67e-ba09-11ed-afa1-0242ac120002"),
+        EquipmentSlot.FEET to UUID.fromString("797ca836-ba09-11ed-afa1-0242ac120002")
+    ))
 
     override fun getMinPower(level: Int): Int {
         return 15 * level
