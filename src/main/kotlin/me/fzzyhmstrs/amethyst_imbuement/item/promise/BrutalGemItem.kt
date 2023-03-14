83775c1fae86cd4f8ca24b8a0a1b98a3c2e1a768
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-03-14 09:35:20 -0400

    Create BrutalGemItem.kt

---
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/promise/BrutalGemItem.kt
@@ -0,0 +1,59 @@
+package me.fzzyhmstrs.amethyst_imbuement.item.promise
+
+import net.minecraft.item.Item
+import net.minecraft.text.Text
+import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterCriteria
+import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterItem
+import me.fzzyhmstrs.fzzy_core.coding_util.AcText
+import me.fzzyhmstrs.fzzy_core.item_util.interfaces.Flavorful
+import net.minecraft.client.item.TooltipContext
+import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.damage.DamageSource
+import net.minecraft.entity.effect.StatusEffect
+import net.minecraft.entity.effect.StatusEffects
+import net.minecraft.entity.mob.HostileEntity
+import net.minecraft.entity.player.PlayerInventory
+import net.minecraft.item.ItemStack
+import net.minecraft.nbt.NbtCompound
+import net.minecraft.registry.Registries
+import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.util.Formatting
+import net.minecraft.world.World
+
+abstract class BrutalGemItem(settings: Settings): IgnitedGemItem(settings) {
+
+    private val HIT_TARGET = 80
+  
+    override fun giveTooltipHint(nbt: NbtCompound, stack: Itemstack, tooltip: MutableList<Text>){
+        if (nbt.contains("mob_hit")){
+            val hit = nbt.getInt("mob_hit").toFloat()
+            val progress = hit/ HIT_TARGET.toFloat()*100.0F
+            tooltip.add(AcText.translatable("item.amethyst_imbuement.gem_of_promise.brutal", progress).append(AcText.literal("%")).formatted(Formatting.GRAY))
+        }
+    }
+    
+    fun brutalGemCheck(stack: ItemStack, inventory: PlayerInventory, damageSource: DamageSource){
+            val source = damageSource.source
+            if (damageSource.name == "mob" || (source != null && source is HostileEntity)){
+                val nbt = stack.orCreateNbt
+                var hit = 0
+                if (nbt.contains("mob_hit")){
+                    hit = nbt.getInt("mob_hit")
+                }
+                val newHit = hit + 1
+                if (newHit >= HIT_TARGET){
+                    stack.decrement(1)
+                    val newStack = ItemStack(RegisterItem.BRUTAL_GEM)
+                    inventory.offerOrDrop(newStack)
+                    val player = inventory.player
+                    if (player is ServerPlayerEntity) {
+                        RegisterCriteria.IGNITE.trigger(player)
+                    }
+                } else {
+                    nbt.putInt("mob_hit",newHit)
+                }
+
+            }
+        }
+
+}
