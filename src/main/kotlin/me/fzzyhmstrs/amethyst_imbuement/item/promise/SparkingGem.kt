c0c8be558cdd5f9a9304bef02525e165be42d5e6
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-03-14 09:20:12 -0400

    Create SparkingGem.kt

---
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/promise/SparkingGem.kt
@@ -0,0 +1,40 @@
+package me.fzzyhmstrs.amethyst_imbuement.item.promise
+
+import net.minecraft.item.Item
+import net.minecraft.text.Text
+import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterCriteria
+import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterItem
+import me.fzzyhmstrs.fzzy_core.coding_util.AcText
+import me.fzzyhmstrs.fzzy_core.item_util.interfaces.Flavorful
+import net.minecraft.client.item.TooltipContext
+import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.damage.DamageSource
+import net.minecraft.entity.effect.StatusEffect
+import net.minecraft.entity.effect.StatusEffects
+import net.minecraft.entity.mob.HostileEntity
+import net.minecraft.entity.player.PlayerInventory
+import net.minecraft.item.ItemStack
+import net.minecraft.nbt.NbtCompound
+import net.minecraft.registry.Registries
+import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.util.Formatting
+import net.minecraft.world.World
+
+abstract class SparkingGemItem(settings: Settings): IgnitedGemItem(settings) {
+
+    override fun giveTooltipHint(nbt: NbtCompound, stack: Itemstack, tooltip: MutableList<Text>){
+    }
+    
+    fun sparkingGemCheck(stack: ItemStack, inventory: PlayerInventory, damageSource: DamageSource){
+            if (damageSource == DamageSource.LIGHTNING_BOLT){
+                stack.decrement(1)
+                val newStack = ItemStack(RegisterItem.SPARKING_GEM)
+                inventory.offerOrDrop(newStack)
+                val player = inventory.player
+                if (player is ServerPlayerEntity) {
+                    RegisterCriteria.IGNITE.trigger(player)
+                }
+            }
+        }
+
+}
