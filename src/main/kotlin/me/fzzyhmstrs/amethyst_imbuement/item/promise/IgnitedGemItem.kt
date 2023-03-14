6860bf5404f2a84ce8416fb7775b991cc7a6c5fb
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-03-14 09:10:04 -0400

    Update IgnitedGemItem.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/promise/IgnitedGemItem.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/promise/IgnitedGemItem.kt
@@ -2,9 +2,11 @@ package me.fzzyhmstrs.amethyst_imbuement.item.promise
 
 import net.minecraft.item.Item
 import net.minecraft.text.Text
+import net.minecraft.item.ItemStack
+import net.minecraft.nbt.NbtCompound
 
 abstract class IgnitedGemItem(settings: Settings): Item(settings) {
 
-    abstract fun giveTooltipHint():List<Text>
+    abstract fun giveTooltipHint(nbt: NbtCompound, stack: ItemStack):List<Text>
 
 }
