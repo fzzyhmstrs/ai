68ba8577213f4b6ad065afe8e8a916792a17abd4
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-03-14 09:24:41 -0400

    Create HealersGemItem.kt
    (cherry picked from commit c2efcf37f5957fe40355beb069017e032175af1c)

---
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/promise/HealersGemItem.kt
@@ -0,0 +1,59 @@
+package me.fzzyhmstrs.amethyst_imbuement.item.promise
+
+import net.minecraft.item.Item
+import net.minecraft.text.Text
+import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterCriteria
+import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterItem
+import me.fzzyhmstrs.fzzy_core.coding_util.AcText
+import me.fzzyhmstrs.fzzy_core.item_util.interfaces.Flavorful
+import net.minecraft.client.item.TooltipContext
+import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.damage.DamageSource
+import net.minecraft.entity.effect.StatusEffect
+import net.minecraft.entity.effect.StatusEffects
+import net.minecraft.entity.mob.HostileEntity
+import net.minecraft.entity.player.PlayerInventory
+import net.minecraft.item.ItemStack
+import net.minecraft.nbt.NbtCompound
+import net.minecraft.registry.Registries
+import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.util.Formatting
+import net.minecraft.world.World
+
+abstract class HealersGemItem(settings: Settings): IgnitedGemItem(settings) {
+
+    override fun giveTooltipHint(nbt: NbtCompound, stack: Itemstack, tooltip: MutableList<Text>){
+        if (nbt.contains("healed")){
+            val healed = nbt.getFloat("healed")
+            val progress = healed/ HEAL_TARGET *100.0F
+            tooltip.add(AcText.translatable("item.amethyst_imbuement.gem_of_promise.healers", progress).append(AcText.literal("%")).formatted(Formatting.GREEN))
+        }
+    }
+    
+    fun healersGemCheck(stack: ItemStack, inventory: PlayerInventory, healAmount: Float){
+            val nbt = stack.orCreateNbt
+            val player = inventory.player
+            if (!player.hungerManager.isNotFull){
+                nbt.putBoolean("healed_with_food",true)
+            }
+            var healed = 0.0F
+            if (nbt.contains("healed")){
+                healed = nbt.getFloat("healed")
+            }
+            val newHeal = healed + healAmount
+            if (newHeal >= HEAL_TARGET){
+                stack.decrement(1)
+                val newStack = ItemStack(RegisterItem.HEALERS_GEM)
+                inventory.offerOrDrop(newStack)
+                if (player is ServerPlayerEntity) {
+                    RegisterCriteria.IGNITE.trigger(player)
+                    if (!nbt.contains("healed_with_food")) {
+                        RegisterCriteria.DEVOUT_CLERIC.trigger(player)
+                    }
+                }
+            } else {
+                nbt.putFloat("healed",newHeal)
+            }
+        }
+
+}
