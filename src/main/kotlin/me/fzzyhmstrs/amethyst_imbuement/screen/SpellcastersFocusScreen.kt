1c995b5437c72a0660f72852d68ea49f92989c45
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-03-15 13:31:39 -0400

    Create SpellcastersFocusScreen.kt
    (cherry picked from commit 7068a6f42fb0a874edc6d59a759e90e6c6e6c9cd)

---
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/screen/SpellcastersFocusScreen.kt
@@ -0,0 +1,84 @@
+package me.fzzyhmstrs.amethyst_imbuement.screen
+
+import com.mojang.blaze3d.systems.RenderSystem
+import me.fzzyhmstrs.amethyst_imbuement.AI
+import me.fzzyhmstrs.fzzy_core.coding_util.AcText
+import net.minecraft.client.gui.screen.ingame.HandledScreen
+import net.minecraft.client.render.DiffuseLighting
+import net.minecraft.client.render.GameRenderer
+import net.minecraft.client.util.math.MatrixStack
+import net.minecraft.enchantment.Enchantment
+import net.minecraft.entity.player.PlayerInventory
+import net.minecraft.item.Items
+import net.minecraft.text.StringVisitable
+import net.minecraft.text.Style
+import net.minecraft.text.Text
+import net.minecraft.util.Formatting
+import net.minecraft.util.Identifier
+import org.joml.Matrix4f
+
+
+@Suppress("VARIABLE_WITH_REDUNDANT_INITIALIZER", "SpellCheckingInspection")
+class SpellcastersFocusScreen(handler: SpellcastersFocusScreenHandler, playerInventory: PlayerInventory, title: Text):
+    HandledScreen<SpellcastersFocusScreenHandler>(handler, playerInventory, title) {
+
+    private val texture = Identifier(AI.MOD_ID,"textures/gui/container/disenchanting_table_gui.png")
+    private val player = playerInventory.player
+
+    override fun mouseClicked(mouseX: Double, mouseY: Double, button: Int): Boolean {
+        val i = (width - backgroundWidth) / 2
+        val j = (height - backgroundHeight) / 2
+        for (k in 0..2) {
+            val d = mouseX - (i + 60).toDouble()
+            val e = mouseY - (j + 14 + 19 * k).toDouble()
+            if (d < 0.0 || e < 0.0 || d >= 108.0 || e >= 19.0 || !player.let {
+                    (handler as DisenchantingTableScreenHandler).onButtonClick(
+                        it, k
+                    )
+                }
+            ) continue
+            client?.interactionManager?.clickButton((handler as DisenchantingTableScreenHandler).syncId, k)
+            return true
+        }
+        return super.mouseClicked(mouseX, mouseY, button)
+    }
+
+
+    override fun drawBackground(matrices: MatrixStack, delta: Float, mouseX: Int, mouseY: Int) {
+        DiffuseLighting.disableGuiDepthLighting()
+        RenderSystem.setShader { GameRenderer.getPositionTexProgram() }
+        RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f)
+        RenderSystem.setShaderTexture(0, this.texture)
+        val i = (width - backgroundWidth) / 2
+        val j = (height - backgroundHeight) / 2
+        this.drawTexture(matrices, i, j, 0, 0, backgroundWidth, backgroundHeight)
+        val k = client?.window?.scaleFactor?.toInt()?:1
+        RenderSystem.viewport((width - 320) / 2 * k, (height - 240) / 2 * k, 320 * k, 240 * k)
+        val matrix4f = Matrix4f().translation(-0.34f, 0.23f, 0.0f).perspective(1.5707964f, 1.3333334f, 9.0f, 80.0f)
+        RenderSystem.backupProjectionMatrix()
+        RenderSystem.setProjectionMatrix(matrix4f)
+
+        client?.window?.framebufferWidth?.let { client?.window?.framebufferHeight?.let { it1 ->
+            RenderSystem.viewport(0, 0, it,
+                it1
+            )
+        } }
+
+        RenderSystem.restoreProjectionMatrix()
+        DiffuseLighting.enableGuiDepthLighting()
+    }
+
+
+    override fun render(matrices: MatrixStack?, mouseX: Int, mouseY: Int, delta: Float) {
+        val dlta = client?.tickDelta?:delta
+        this.renderBackground(matrices)
+        super.render(matrices, mouseX, mouseY, dlta)
+        drawMouseoverTooltip(matrices, mouseX, mouseY)
+        for (j in 0..2) {
+            if (!isPointWithinBounds(60, 14 + 19 * j, 108, 17, mouseX.toDouble(), mouseY.toDouble())) continue
+            val tooltipText = TODO()
+            this.renderTooltip(matrices, tooltipText, mouseX, mouseY)
+            break
+        }
+    }
+}
