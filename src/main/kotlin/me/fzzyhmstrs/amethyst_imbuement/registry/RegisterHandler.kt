ef8346a116060b9c0be23369dc7bf3fa7ca04fc9
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-03-15 13:41:57 -0400

    Update RegisterHandler.kt
    (cherry picked from commit 52c7fa3a17c072e40abdacbd13c53ef10c47d1a5)

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/registry/RegisterHandler.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/registry/RegisterHandler.kt
@@ -5,7 +5,9 @@ import me.fzzyhmstrs.amethyst_imbuement.screen.AltarOfExperienceScreenHandler
 import me.fzzyhmstrs.amethyst_imbuement.screen.CrystalAltarScreenHandler
 import me.fzzyhmstrs.amethyst_imbuement.screen.DisenchantingTableScreenHandler
 import me.fzzyhmstrs.amethyst_imbuement.screen.ImbuingTableScreenHandler
+import net.fabricmc.fabric.api.screenhandler.v1.ExtendedScreenHandlerType
 import net.minecraft.entity.player.PlayerInventory
+import net.minecraft.network.PacketByteBuf
 import net.minecraft.screen.ScreenHandlerType
 import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
@@ -15,6 +17,7 @@ object RegisterHandler {
     var ALTAR_OF_EXPERIENCE_SCREEN_HANDLER: ScreenHandlerType<AltarOfExperienceScreenHandler>? = null
     var DISENCHANTING_TABLE_SCREEN_HANDLER: ScreenHandlerType<DisenchantingTableScreenHandler>? = null
     var CRYSTAL_ALTAR_SCREEN_HANDLER: ScreenHandlerType<CrystalAltarScreenHandler>? = null
+    var SPELLCASTERS_FOCUS_SCREEN_HANDLER: ExtendedScreenHandlerType<CrystalAltarScreenHandler>? = null
 
     fun registerAll(){
 
@@ -40,15 +43,24 @@ object RegisterHandler {
         }
 
         CRYSTAL_ALTAR_SCREEN_HANDLER = ScreenHandlerType { syncID: Int, playerInventory: PlayerInventory ->
-        CrystalAltarScreenHandler(
-            syncID,
-            playerInventory
-        )
+            CrystalAltarScreenHandler(
+                syncID,
+                playerInventory
+            )
+        }
+
+        SPELLCASTERS_FOCUS_SCREEN_HANDLER = ExtendedScreenHandlerType { syncID: Int, playerInventory: PlayerInventory, buf: PacketByteBuf ->
+            CrystalAltarScreenHandler(
+                syncID,
+                playerInventory,
+                buf
+            )
         }
 
         Registry.register(Registry.SCREEN_HANDLER,Identifier(AI.MOD_ID,"imbuing_table"), IMBUING_SCREEN_HANDLER)
         Registry.register(Registry.SCREEN_HANDLER,Identifier(AI.MOD_ID,"altar_of_experience"), ALTAR_OF_EXPERIENCE_SCREEN_HANDLER)
         Registry.register(Registry.SCREEN_HANDLER,Identifier(AI.MOD_ID,"disenchanting_table"), DISENCHANTING_TABLE_SCREEN_HANDLER)
         Registry.register(Registry.SCREEN_HANDLER,Identifier(AI.MOD_ID,"crystal_altar"), CRYSTAL_ALTAR_SCREEN_HANDLER)
+        Registry.register(Registry.SCREEN_HANDLER,Identifier(AI.MOD_ID,"spellcasters_focus"), SPELLCASTERS_FOCUS_SCREEN_HANDLER)
     }
 }
