5441f858a46228146ce6b0545dd38b62523f9300
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-03-16 15:01:20 -0400

    Update NewAiConfig.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/config/NewAiConfig.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/config/NewAiConfig.kt
@@ -58,13 +58,123 @@ object NewAiConfig
             @ReadMeText("readme.items.lethalityDamage")
             var lethalityDamage = ValidatedFloat(LethalityToolMaterial.defaultAttackDamage(),30f,0f)
         }
-
-
+    }
+    
+    private val altarsHeader = buildSectionHeader("altars")
+    
+    class Altars: ConfigClass(altarsHeader){
+        var xpBush: XpBush()
+        class XpBush: ConfigSection(Header.Builder().space().add("readme.altars.xp_bush_1").add("readme.altars.xp_bush_2").build()){
+            var bonemealChance = ValidatedFloat(0.4f,1f,0f)
+            var growChance = ValidatedFloat(0.15f,1f,0f)
+        }
+        
+        var disenchanter: Disenchanter()
+        class Disenchanter: ConfigSection(Header.Builder().space().add("readme.altars.disenchant_1").add("readme.altars.disenchant_2").build()){
+            var levelCosts = ValidatedIntList(listOf(11, 17, 24, 33, 44), {i -> i >= 0}, "Needs integers greater than or equal to 0")
+            var baseDisenchantsAllowed = ValidatedInt(1,Int.MAX_VALUE,0)
+        }
+        
+        var imbuing: Imbuing()
+        class Imbuing: ConfigSection(Header.Builder().space().add("readme.items.imbuing_1").add("readme.items.imbuing_2").build()){
+            var enchantingEnabled = ValidatedBoolean(true)
+            var replaceEnchantingTable = ValidatedBoolean(false)
+            @ReadMeText("readme.altars.imbuing.difficultyModifier")
+            var difficultyModifier = ValidatedFloat(1.0F,10f,0f)
+            
+            var easyMagic = EasyMagic()
+            class EasyMagic: ConfigSection(Header.Builder().add("readme.items.imbuing_easy_1").add("readme.items.imbuing_easy_2").build()){
+                var matchEasyMagicBehavior = ValidatedBoolean(true)
+                var rerollEnabled = ValidatedBoolean(true)
+                var levelCost = ValidatedInt(5,Int.MAX_VALUE,0)
+                var lapisCost = ValidatedInt(1,Int.MAX_VALUE,0)
+                var showTooltip = ValidatedBoolean(true)
+                var singleEnchantTooltip = ValidatedBoolean(true)
+            }
+            
+            var reroll: Reroll()
+            class Reroll:ConfigSection(Header.Builder().add("readme.items.imbuing_reroll_1").add("readme.items.imbuing_reroll_2").build()){
+                var matchRerollBehavior = ValidatedBoolean(true)
+                var levelCost = ValidatedInt(1,Int.MAX_VALUE,0)
+                var lapisCost = ValidatedInt(0,INT.MAX_VALUE,0)
+            }
+        }
+    
+        var altar: Altar()
+        class Altar: ConfigSection(Header.Builder().space().add("readme.altars.altar_1").add("readme.altars.altar_2").build()){
+            var baseLevels: Int = 35
+            var candleLevelsPer: Int = 5
+            @ReadMeText("readme.altars.altar.customXpMethod")
+            var customXpMethod: Boolean = true
+        }
     }
 
+    private val villagesHeader = buildSectionHeader("villages")
+    
+    class Villages: ConfigClass(villagesHeader){
+        var vanilla: Vanilla()
+        class Vanilla: ConfigSection(Header.Builder().space().add("readme.villages.vanilla_1").add("readme.villages.vanilla_2").build()){
+            var enableDesertWorkshops = ValidatedBoolean(true)
+            var desertWorkshopWeight = ValidatedInt(2,100,1)
+            var enablePlainsWorkshops = ValidatedBoolean(true)
+            var plainsWorkshopWeight = ValidatedInt(3,100,1)
+            var enableSavannaWorkshops = ValidatedBoolean(true)
+            var savannaWorkshopWeight = ValidatedInt(3,100,1)
+            var enableSnowyWorkshops = ValidatedBoolean(true)
+            var snowyWorkshopWeight = ValidatedInt(2,100,1)
+            var enableTaigaWorkshops = ValidatedBoolean(true)
+            var taigaWorkshopWeight = ValidatedInt(3,100,1)
+        }
+        
+        var ctov: Ctov()
+        class Ctov: ConfigSection(Header.Builder().space().add("readme.villages.ctov_1").add("readme.villages.ctov_2").build()){
+            var enableCtovWorkshops = ValidatedBoolean(true)
+            var beachWorkshopWeight = ValidatedInt(4,100,1)
+            var darkForestWorkshopWeight = ValidatedInt(4,100,1)
+            var jungleWorkshopWeight = ValidatedInt(4,100,1)
+            var jungleTreeWorkshopWeight = ValidatedInt(4,100,1)
+            var mesaWorkshopWeight = ValidatedInt(4,100,1)
+            var mesaFortifiedWorkshopWeight = ValidatedInt(4,100,1)
+            var mountainWorkshopWeight = ValidatedInt(4,100,1)
+            var mountainAlpineWorkshopWeight = ValidatedInt(4,100,1)
+            var mushroomWorkshopWeight = ValidatedInt(4,100,1)
+            var swampWorkshopWeight = ValidatedInt(4,100,1)
+            var swampFortifiedWorkshopWeight = ValidatedInt(4,100,1)
+        }
+        
+        var rs: Rs()
+        class Rs: ConfigSection(Header.Builder().space().add("readme.villages.ctov_1").add("readme.villages.ctov_2").build()){
+            var enableRsWorkshops = ValidatedBoolean(true)
+            var badlandsWorkshopWeight = ValidatedInt(2,100,1)
+            var birchWorkshopWeight = ValidatedInt(2,100,1)
+            var darkForestWorkshopWeight = ValidatedInt(2,100,1)
+            var giantTaigaWorkshopWeight = ValidatedInt(1,100,1)
+            var jungleWorkshopWeight = ValidatedInt(2,100,1)
+            var mountainsWorkshopWeight = ValidatedInt(2,100,1)
+            var mushroomsWorkshopWeight = ValidatedInt(2,100,1)
+            var oakWorkshopWeight = ValidatedInt(2,100,1)
+            var swampWorkshopWeight = ValidatedInt(2,100,1)
+            var crimsonWorkshopWeight = ValidatedInt(2,100,1)
+            var warpedWorkshopWeight = ValidatedInt(2,100,1)
+        }
+    }
+    
+    private val enchantsHeader = buildSectionHeader("enchants")
+    
+    class Enchants: Villages: ConfigClass(enchantsHeader){
+        
+        @ReadMeText("readme.enchants.disableExtraEnchantLevels")
+        var disableExtraEnchantLevels = ValidatedBoolean(false)
+        
+        @ReadMeText("readme.enchants.enabledEnchants")
+        var enabledEnchants = ValidatedStringBoolMap(AiConfigDefaults.enabledEnchantments,{id,_ -> Registries.ENCHANTMENT.containsId(Identifier.tryParse(id))}, "Needs a valid registered enchantment identifier.")
+        
+        @ReadMeText("readme.enchants.enchantCosts")
+        var enchantCosts = ValidatedStringIntMap(AiConfigDefaults.enchantmentCosts,{id,i -> Registries.ENCHANTMENT.containsId(Identifier.tryParse(id)) && i > 0}, "Needs a valid registered enchantment identifier and a level greater than 0.")
+    }
 
     private fun buildSectionHeader(name:String): Header{
-        return Header.Builder().underoverscore("readme.header.$name").add("readme.header.$name.desc").space().build()
+        return Header.Builder().space().underoverscore("readme.header.$name").add("readme.header.$name.desc").space().build()
     }
 
 }
