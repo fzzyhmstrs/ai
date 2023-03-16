a776b10980d93b16b58805be6ba516f2d6978a4b
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-03-16 10:17:05 -0400

    Update SpellcastersReagent.kt
    (cherry picked from commit af43133f20765845e4ba8a9363d004ed499b7d6e)

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/SpellcastersReagent.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/SpellcastersReagent.kt
@@ -3,7 +3,8 @@ package me.fzzyhmstrs.amethyst_imbuement.item
 import net.minecraft.inventory.Inventory
 import net.minecraft.item.ItemStack
 import net.minecraft.entity.attribute.EntityAttributeModifier
+import net.minecraft.entity.attribute.EntityAttribute
 
 interface SpellcastersReagent: Reagent{
-    fun getAttributeModifier(): EntityAttributeModifier
+    fun getAttributeModifier(): Pair<Attribute,EntityAttributeModifier>
 }
