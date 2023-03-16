7f33d090a001bb3ceb8b5ccac51ce21d58323324
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-03-16 10:18:11 -0400

    Update SpellcastersReagentItem.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/SpellcastersReagentItem.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/SpellcastersReagentItem.kt
@@ -4,11 +4,19 @@ import net.minecraft.item.Item
 import net.minecraft.text.Text
 import net.minecraft.item.ItemStack
 import net.minecraft.nbt.NbtCompound
+import net.minecraft.entity.attribute.EntityAttributeModifier
+import net.minecraft.entity.attribute.EntityAttribut
 
-class SpellcastersReagentItem(private val attribute: EntityAttributeModifier,settings: Settings): Item(settings),SpellcastersReagent {
+class SpellcastersReagentItem(
+    private val attribute: EntityAttribute,
+    private val modifier: EntityAttributeModifier,
+    settings: Settings)
+    : 
+    Item(settings),SpellcastersReagent 
+{
 
-    override fun getAttributeModifier(): EntityAttributeModifier{
-        return attribute
+    override fun getAttributeModifier(): Pair<EntityAttribute,EntityAttributeModifier>{
+        return Pair(attribute,modifier)
     }
 
 }
