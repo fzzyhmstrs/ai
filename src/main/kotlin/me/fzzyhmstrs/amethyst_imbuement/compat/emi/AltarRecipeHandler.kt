f711d44a422505d93ad4745164801fdbeb53bb73
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-03-22 09:22:26 -0400

    Update AltarRecipeHandler.kt
    (cherry picked from commit eef89a69140678e1d941abee6dbe659039be586c)

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/compat/emi/AltarRecipeHandler.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/compat/emi/AltarRecipeHandler.kt
@@ -7,19 +7,19 @@ import net.minecraft.screen.slot.Slot
 
 class AltarRecipeHandler: EmiRecipeHandler<CrystalAltarScreenHandler> {
     override fun getInputSources(handler: CrystalAltarScreenHandler): MutableList<Slot> {
-        val list: MutableList<Slot> = mutableListOf(handler.slots[0],handler.slots[1])
-        for (i in 3 until 39){
+        val list: MutableList<Slot> = mutableListOf(handler.slots[0],handler.slots[1],handler.slots[2])
+        for (i in 4 until 40){
             list.add(handler.slots[i])
         }
         return list
     }
 
     override fun getCraftingSlots(handler: CrystalAltarScreenHandler): MutableList<Slot> {
-        return mutableListOf(handler.slots[0],handler.slots[1])
+        return mutableListOf(handler.slots[0],handler.slots[1],handler.slots[2])
     }
 
     override fun getOutputSlot(handler: CrystalAltarScreenHandler): Slot? {
-        return handler.getSlot(2)
+        return handler.getSlot(3)
     }
 
     override fun supportsRecipe(recipe: EmiRecipe): Boolean {
