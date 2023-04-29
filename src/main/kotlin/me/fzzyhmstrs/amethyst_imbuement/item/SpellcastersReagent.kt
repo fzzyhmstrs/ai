04f22cb8f49fd41ebbc4821b3bbb7945c39454e6
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-04-29 13:45:50 -0400

    fix issue with attributes not stacking. pull out the ol' random uuid again...

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/SpellcastersReagent.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/SpellcastersReagent.kt
@@ -2,7 +2,24 @@ package me.fzzyhmstrs.amethyst_imbuement.item
 
 import net.minecraft.entity.attribute.EntityAttribute
 import net.minecraft.entity.attribute.EntityAttributeModifier
+import net.minecraft.nbt.NbtCompound
+import net.minecraft.registry.Registries
+import java.util.*
 
 interface SpellcastersReagent: Reagent{
     fun getAttributeModifier(): Pair<EntityAttribute,EntityAttributeModifier>
+
+    companion object{
+        fun toNbt(attribute: Pair<EntityAttribute,EntityAttributeModifier>): NbtCompound {
+            val nbtCompound = NbtCompound()
+            nbtCompound.putString("Name", attribute.second.name)
+            nbtCompound.putDouble("Amount", attribute.second.value)
+            nbtCompound.putInt("Operation", attribute.second.operation.id)
+            nbtCompound.putUuid("UUID", UUID.randomUUID())
+            nbtCompound.putString("AttributeName", Registries.ATTRIBUTE.getId(attribute.first).toString())
+            return nbtCompound
+        }
+
+
+    }
 }
