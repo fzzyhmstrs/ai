1404be4e904fa7d1d524d0273b34dc32b8921d10
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-06-06 10:59:33 -0400

    Update BaseHamsterEntity.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/entity/living/BaseHamsterEntity.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/entity/living/BaseHamsterEntity.kt
@@ -1,5 +1,6 @@
 package me.fzzyhmstrs.amethyst_imbuement.entity.living
 
+import me.fzzyhmstrs.amethyst_core.interfaces.SpellCastingEntity
 import me.fzzyhmstrs.amethyst_core.modifier_util.AugmentEffect
 import me.fzzyhmstrs.amethyst_imbuement.config.AiConfig
 import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterArmor
@@ -18,12 +19,11 @@ import net.minecraft.world.LocalDifficulty
 import net.minecraft.world.World
 
 @Suppress("PrivatePropertyName")
-open class BaseHamsterEntity: PlayerCreatedConstructEntity {
+open class BaseHamsterEntity: PlayerCreatedConstructEntity, SpellCastingEntity {
 
     constructor(entityType: EntityType<BaseHamsterEntity>, world: World): super(entityType, world)
 
-    constructor(entityType: EntityType<BaseHamsterEntity>, world: World, ageLimit: Int, createdBy: LivingEntity? = null, augmentEffect: AugmentEffect? = null, level: Int = 1, bonusEquipment: Int = 0) : super(entityType, world, ageLimit, createdBy, augmentEffect, level){
-        this.bonusEquipment = bonusEquipment
+    constructor(entityType: EntityType<BaseHamsterEntity>, world: World, ageLimit: Int, createdBy: LivingEntity? = null, augmentEffect: AugmentEffect? = null, level: Int = 1) : super(entityType, world, ageLimit, createdBy, augmentEffect, level){
     }
 
     companion object {
@@ -38,13 +38,20 @@ open class BaseHamsterEntity: PlayerCreatedConstructEntity {
         }
     }
 
-    private var bonusEquipment = 0
-
     override fun initGoals() {
         super.initGoals()
         goalSelector.add(5, ConstructLookGoal(this))
     }
 
+    override fun getRotationVec3d(): Vec3d{
+        if (this.target != null){
+            val vec1 = target.pos.add(0.0,target.height.toDouble()/2.0,0.0)
+            val vec2 = this.getEyePos()
+            return vec1.subtract(vec2).normalize()
+        } else {
+            return this.getRotationVector()
+        }
+    }
 
     override fun getAmbientSound(): SoundEvent? {
         return RegisterSound.HAMSTER_AMBIENT
@@ -62,8 +69,14 @@ open class BaseHamsterEntity: PlayerCreatedConstructEntity {
         return SoundEvents.ENTITY_TURTLE_SHAMBLE_BABY
     }
 
+    open fun classEquipment(): Map<EquipmentSlot, ItemStack>{
+        return mapOf()
+    }
+    
     override fun initEquipment(random: Random ,difficulty: LocalDifficulty) {
-        this.equipStack(EquipmentSlot.MAINHAND, ItemStack(RegisterItem.GLOWING_BLADE))
+        for (entry in classEquipment){
+            this.equipStack(entry.key, entry.value.copy())
+        }
     }
 
 }
