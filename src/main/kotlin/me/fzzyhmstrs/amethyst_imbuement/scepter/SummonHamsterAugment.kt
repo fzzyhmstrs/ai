b9a74997113fae26b9a853205394224ea2a2da0a
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-06-06 13:04:41 -0400

    Create SummonHamsterAugment.kt
    (cherry picked from commit d473f8811786a6d769ea16a5008351289c922ef3)

---
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/scepter/SummonHamsterAugment.kt
@@ -0,0 +1,55 @@
+package me.fzzyhmstrs.amethyst_imbuement.scepter
+
+import me.fzzyhmstrs.amethyst_core.modifier_util.AugmentEffect
+import me.fzzyhmstrs.amethyst_core.scepter_util.LoreTier
+import me.fzzyhmstrs.amethyst_core.scepter_util.ScepterTier
+import me.fzzyhmstrs.amethyst_core.scepter_util.SpellType
+import me.fzzyhmstrs.amethyst_core.scepter_util.augments.AugmentDatapoint
+import me.fzzyhmstrs.amethyst_core.scepter_util.augments.SummonEntityAugment
+import me.fzzyhmstrs.amethyst_imbuement.config.AiConfig
+import me.fzzyhmstrs.amethyst_imbuement.entity.living.CrystallineGolemEntity
+import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterEntity
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.item.Items
+import net.minecraft.sound.SoundEvent
+import net.minecraft.sound.SoundEvents
+import net.minecraft.util.hit.BlockHitResult
+import net.minecraft.util.hit.HitResult
+import net.minecraft.world.World
+
+@Suppress("SpellCheckingInspection")
+class SummonHamsterAugment: SummonEntityAugment(ScepterTier.ONE,10) {
+
+    override val baseEffect: AugmentEffect
+        get() = super.baseEffect
+            .withDuration(AiConfig.entities.hamster.baseLifespan.get())
+            .withAmplifier(AiConfig.entities.hamster.baseHealth.get().toInt())
+            .withDamage(AiConfig.entities.hamster.baseDamage.get(),AiConfig.entities.hamster.perLvlDamage.get())
+
+    override fun augmentStat(imbueLevel: Int): AugmentDatapoint {
+        return AugmentDatapoint(SpellType.WIT,600,50,
+            1,imbueLevel,10,LoreTier.NO_TIER, Items.WHITE_WOOL)
+    }
+
+    override fun placeEntity(
+        world: World,
+        user: PlayerEntity,
+        hit: HitResult,
+        level: Int,
+        effects: AugmentEffect
+    ): Boolean {
+        val startPos = (hit as BlockHitResult).blockPos
+        val spawnPos = findSpawnPos(world,startPos,3,1, tries = 12)
+        val hampter = BaseHamsterEntity(RegisterEntity.BASIC_HAMSTER_ENTITY, world, effects.duration(level), user, effects, level)
+        hampter.setPos(spawnPos.x +0.5, spawnPos.y +0.05, spawnPos.z + 0.5)
+        hampter.refreshPositionAndAngles(spawnPos.x +0.5, spawnPos.y +0.05, spawnPos.z + 0.5,(world.random.nextFloat() * 360f) - 180f,user.pitch)
+        if (world.spawnEntity(hampter)) {
+            return super.placeEntity(world, user, hit, level, effects)
+        }
+        return false
+    }
+
+    override fun soundEvent(): SoundEvent {
+        return RegisterSound.HAMSTER_AMBIENT
+    }
+}
