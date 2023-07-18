dc1f867285391bb112ccd0c5409706caf1acbe05
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-07-18 12:59:35 -0400

    Update BodySwapAugment.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/spells/BodySwapAugment.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/spells/BodySwapAugment.kt
@@ -27,7 +27,7 @@ import net.minecraft.util.math.Vec3d
 import net.minecraft.world.World
 import java.util.*
 
-class BodySwapAugment: SingleTargetAugment(ScepterTier.THREE){
+class BodySwapAugment: ScepterAugment(ScepterTier.THREE,AugmentType.SINGLE_TARGET){
     override val augmentData: AugmentDatapoint =
         AugmentDatapoint(AI.identity("body_swap"),SpellType.FURY,600,125,
             19,11,1,40, LoreTier.HIGH_TIER, Items.ENDER_EYE)
@@ -37,11 +37,18 @@ class BodySwapAugment: SingleTargetAugment(ScepterTier.THREE){
         get() = super.baseEffect.withRange(15.75,0.75)
 
     override fun appendDescription(description: MutableList<Text>, other: ScepterAugment, othersType: AugmentType) {
-        TODO("Not yet implemented")
+        when(other) {
+            RegisterEnchantment.EMPOWERED_SLASH -> 
+                description.addLang("enchantment.amethyst_imbuement.body_swap.empowered_slash.desc", SpellAdvancementChecks.UNIQUE.or(SpellAdvancementChecks.DAMAGE))
+            RegisterEnchantment.CURSE ->
+                description.addLang("enchantment.amethyst_imbuement.body_swap.curse.desc", SpellAdvancementChecks.UNIQUE)
+        }
+        if (othersType.has(AugmentType.PROJECTILE))
+            description.addLang("enchantment.amethyst_imbuement.body_swap.desc.projectile", SpellAdvancementChecks.ON_KILLED)
     }
 
     override fun provideArgs(pairedSpell: ScepterAugment): Array<Text> {
-        TODO()
+        return arrayOf(pairedSpell.provideNoun(this))
     }
 
     override fun onPaired(player: ServerPlayerEntity, pair: PairedAugments) {
@@ -51,19 +58,41 @@ class BodySwapAugment: SingleTargetAugment(ScepterTier.THREE){
         if (pair.spellsAreUnique()){
             SpellAdvancementChecks.grant(player, SpellAdvancementChecks.UNIQUE_TRIGGER)
         }
+        SpellAdvancementChecks.grant(player, SpellAdvancementChecks.ON_KILLED_TRIGGER)
     }
 
-    override fun effect(
+    override fun specialName(otherSpell: ScepterAugment): MutableText {
+        return when (otherSpell) {
+            RegisterEnchantment.EMPOWERED_SLASH ->
+                AcText.translatable("enchantment.amethyst_imbuement.body_swap.empowered_slash")
+            RegisterEnchantment.FANGS ->
+                AcText.translatable("enchantment.amethyst_imbuement.body_swap.curse")
+            else ->
+                super.specialName(otherSpell)
+        }
+    }
+
+    override fun <T> applyTasks(
         world: World,
-        target: Entity?,
-        user: LivingEntity,
+        context: ProcessContext,
+        user: T,
+        hand: Hand,
         level: Int,
-        hit: HitResult?,
-        effect: AugmentEffect
-    ): Boolean {
-        if (world !is ServerWorld) return false
-        if (user !is PlayerEntity) return false
-        val rotation = user.getRotationVec(1.0F)
+        effects: AugmentEffect,
+        spells: PairedAugments
+    )
+    :
+    SpellActionResult
+    where
+    T : SpellCastingEntity,
+    T : LivingEntity
+    {
+        val onCastResults = spells.processOnCast(context,world,null,user, hand, level, effects)
+        if (!onCastResults.success()) return FAIL
+        if (onCastResults.overwrite()) return onCastResults
+
+        if (world !is ServerWorld) return FAIL
+        val rotation = user.getRotationVec3d()
         val perpendicularVector = RaycasterUtil.perpendicularVector(rotation, RaycasterUtil.InPlane.XZ)
         val raycasterPos = user.pos.add(rotation.multiply(effect.range(level)/2)).add(Vec3d(0.0,user.height/2.0,0.0))
         val entityList: MutableList<Entity> =
@@ -76,21 +105,88 @@ class BodySwapAugment: SingleTargetAugment(ScepterTier.THREE){
                 effect.range(level),
                 2.5,
                 3.0)
-        if (entityList.isEmpty()) return false
+        if (entityList.isEmpty()) return FAIL
+        val targetedEntityList = entityList.stream().filter { it is LivingEntity && if (it is SpellCastingEntity) AiConfig.entities.isEntityPvpTeammate(user,it,this) else true }.toList()
+        if (targetedEntityList.isEmpty()) return FAIL
         val map: SortedMap<Double,Entity> = sortedMapOf()
-        entityList.forEach {
+        targetedEntityList.forEach {
             map[it.squaredDistanceTo(user)] = it
         }
-        val entity = map[map.lastKey()]?:return false
-        val pos1 = Vec3d(entity.pos.toVector3f())
-        val pos2 = Vec3d(user.pos.toVector3f())
-        entity.teleport(pos2.x,pos2.y,pos2.z)
-        user.teleport(pos1.x,pos1.y,pos1.z)
-        world.playSound(null, user.blockPos, soundEvent(), SoundCategory.PLAYERS, 0.7F, 1.1F)
-        return true
+        val entity = map[map.lastKey()]?:return FAIL
+        val list = spells.processSingleEntityHit(EntityHitResult(entity),context,world,null,user, hand, level, effects)
+        list.addAll(onCastResults.results())
+        return if (list.isEmpty()) {
+            FAIL
+        } else {
+            spells.castSoundEvents(world,user.blockPos,context)
+            SpellActionResult.success(list)
+        }
     }
 
-    override fun soundEvent(): SoundEvent {
-        return SoundEvents.BLOCK_PORTAL_TRIGGER
+    override fun <T> onEntityHit(
+        entityHitResult: EntityHitResult,
+        context: ProcessContext,
+        world: World,
+        source: Entity?,
+        user: T,
+        hand: Hand,
+        level: Int,
+        effects: AugmentEffect,
+        othersType: AugmentType,
+        spells: PairedAugments
+    )
+    :
+    SpellActionResult
+    where
+    T : SpellCastingEntity,
+    T : LivingEntity
+    {
+        if (othersType.empty){
+            val pos0 = entityHitResult.entity.pos
+            val pos1 = Vec3d(pos0.toVector3f())
+            val pos2 = Vec3d(user.pos.toVector3f())
+            splashParticles(entityHitResult,world,pos0.x,pos0.y,pos0.z,spells)
+            if (world is ServerWorld){
+                val particle = spells.getCastParticleType()
+                world.spawnParticles(particle,pos0.x,pos0.y,pos0.z,20,.25,.25,.25,0.2)
+            }
+            entity.teleport(pos2.x,pos2.y,pos2.z)
+            user.teleport(pos1.x,pos1.y,pos1.z)
+            return 
+        }
+        return SUCCESSFUL_PASS
+    }
+
+    override fun <T> onEntityKill(
+        entityHitResult: EntityHitResult,
+        context: ProcessContext,
+        world: World,
+        source: Entity?,
+        user: T,
+        hand: Hand,
+        level: Int,
+        effects: AugmentEffect,
+        othersType: AugmentType,
+        spells: PairedAugments
+    ): SpellActionResult where T : SpellCastingEntity,T : LivingEntity {
+        if (othersType.has(AugmentType.PROJECTILE)){
+            val pos0 = entityHitResult.entity.pos
+            val pos1 = Vec3d(pos0.toVector3f())
+            user.teleport(pos1.x,pos1.y,pos1.z)
+            SpellActionResult.success(AugmentHelper.DAMAGED_MOB
+        }
+        return SUCCESSFUL_PASS
+    }
+
+    override hitParticleType(): ParticleEffect?{
+        return ParticleTypes.PORTAL
+    }
+    
+    override hitParticleType(hit: HitResult): ParticleEffect?{
+        return ParticleTypes.PORTAL
+    }
+
+    override fun castSoundEvent(world: World, blockPos: BlockPos, context: ProcessContext){
+        world.playSound(null, blockPos, SoundEvents.BLOCK_PORTAL_TRIGGER, SoundCategories.PLAYER, 0.7F, 1.1F)
     }
 }
