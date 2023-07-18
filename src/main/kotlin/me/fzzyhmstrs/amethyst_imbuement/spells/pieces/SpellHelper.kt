f202d8fd670f91a7db8ae0d5ac53fbc2dd4c435e
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-07-18 11:10:32 -0400

    Update SpellHelper.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/spells/pieces/SpellHelper.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/spells/pieces/SpellHelper.kt
@@ -22,4 +22,32 @@ object SpellHelper {
         return (target is Monster || target is SpellCastingEntity && !AiConfig.entities.isEntityPvpTeammate(user,target,spell))
     }
 
+    fun hostileFilter(list: List<Entity>, user: LivingEntity, spell: ScepterAugment): MutableList<EntityHitResult> {
+        val hostileEntityList: MutableList<EntityHitResult> = mutableListOf()
+        if (list.isNotEmpty()) {
+            for (entity in list) {
+                if (entity !== user) {
+                    if (entity is PlayerEntity && !getPvpMode()) continue
+                    if (entity is SpellCastingEntity && AiConfig.entities.isEntityPvpTeammate(user,entity,spell)) continue
+                    hostileEntityList.add(EntityHitResult(entity))
+                }
+            }
+        }
+        return hostileEntityList
+    }
+
+    fun friendlyFilter(list: List<Entity>, user: LivingEntity, spell: ScepterAugment): MutableList<EntityHitResult> {
+        val friendlyEntityList: MutableList<EntityHitResult> = mutableListOf()
+        if (list.isNotEmpty()) {
+            for (entity in list) {
+                if (entity !== user) {
+                    if (entity is Monster) continue
+                    if (entity is SpellCastingEntity && !AiConfig.entities.isEntityPvpTeammate(user,entity,spell)) continue
+                    friendlyEntityList.add(EntityHitResult(entity))
+                }
+            }
+        }
+        return friendlyEntityList
+    }
+    
 }
