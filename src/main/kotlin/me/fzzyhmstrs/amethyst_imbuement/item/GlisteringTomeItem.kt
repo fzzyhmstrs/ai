2f34b9a5d79d079c26b4d4c0bc48cf6833c018b3
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-07-18 15:52:12 -0400

    re-add patchouli book

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/GlisteringTomeItem.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/GlisteringTomeItem.kt
@@ -1,12 +1,18 @@
 package me.fzzyhmstrs.amethyst_imbuement.item
 
+import me.fzzyhmstrs.amethyst_imbuement.AI
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
+import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.util.Hand
+import net.minecraft.util.TypedActionResult
+import net.minecraft.world.World
+import vazkii.patchouli.api.PatchouliAPI
 
 
 class GlisteringTomeItem(settings: Settings): Item(settings) {
     override fun use(world: World, playerEntity: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
-        import vazkii.patchouli.api.PatchouliAPI
-
         if (!world.isClient) {
             PatchouliAPI.get().openBookGUI(playerEntity as ServerPlayerEntity, AI.identity("glistering_tome"))
             return TypedActionResult.success(playerEntity.getStackInHand(hand))
