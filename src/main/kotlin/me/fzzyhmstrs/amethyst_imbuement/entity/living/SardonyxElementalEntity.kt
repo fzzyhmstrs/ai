693ca18df3bf92e8df4124d95c09d5b6228ca0b8
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-08-01 15:00:09 -0400

    Update SardonyxElementalEntity.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/entity/living/SardonyxElementalEntity.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/entity/living/SardonyxElementalEntity.kt
@@ -7,13 +7,22 @@ import net.minecraft.world.World
 class SardonyxElementalEntity(entityType: EntityType<out AttackTicksGolemEntity>, world: World): AttackTicksGolemEntity(entityType, world), Monster {
 
     /*
-    Attacks
+    Attacks and Spells
     Melee - Slow, powerful
     AOE Spikes - expanding rings of spikes like the illager AOE attack. Announced with a special sound. 25 Mana
     Chest Laser Beam - extremely damaging beam, telegraphs it's direction for a bit before. 50 mana
-    Crystal Shard Fan - relatively fast ranged attack, 3 sardonyx shards. 5 mana
+    Crystal Shard Fan - relatively fast ranged attack, 3 sardonyx shards. 5 Mana
+    Crystal Shell - Temporary Resist 4. 25 Mana
+    Summon Fragments - Summons Fragments around it based on Config number. 25 Mana
+    Reflect - Used when taking massive DPS, relects taken damage to dealer, reduces damage taken by half while active. 25 Mana
 
     May want to build in a "memory" of magic users, increasing damage against them.
+
+    Drops: Sardonyx Figurine, used to slowly spawn Sardonyx Fragments
+    Sardonyx Fragments are the mobs that drop actual Sardonyx (Elemental may also drop a few, based on looting)
+
+    Regens 10 (X) mana on killing a mob.
+    Regens 2 (X) mana and X health when one of it's fragments dies.
     */
     
     class SardonyxElemental: ConfigSection(Header.Builder().space().add("readme.entities.sardonyx_elemental_1").build()){
@@ -27,7 +36,7 @@ class SardonyxElementalEntity(entityType: EntityType<out AttackTicksGolemEntity>
         @ReadMeText("readme.entities.sardonyx_elemental.baseMana")
         var baseMana = ValidatedInt(100,1500,-1)
         @ReadMeText("readme.entities.sardonyx_elemental.baseManaRegen")
-        var baseManaRegen = ValidatedInt(40,400,1)
+        var baseManaRegen = ValidatedInt(10,100,1)
         @ReadMeText("readme.entities.sardonyx_elemental.thornsDamage")
         var thornsDamage = ValidatedFloat(4.0f,20.0f,0.0f)
         @ReadMeText("readme.entities.sardonyx_elemental.spikesDamage")
