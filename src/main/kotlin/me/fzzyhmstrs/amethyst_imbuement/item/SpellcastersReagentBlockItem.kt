d4b48bb28ddcd363f70e69ed8ff3e35cb8670ef6
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-08-02 13:11:28 -0400

    Update SpellcastersReagentBlockItem.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/SpellcastersReagentBlockItem.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/SpellcastersReagentBlockItem.kt
@@ -11,11 +11,63 @@ class SpellcastersReagentBlockItem(
     block: Block,
     settings: Settings)
     : 
-    BlockItem(block,settings), SpellcastersReagent
+    BlockItem(block,settings), SpellcastersReagent, Flavorful<SpellcastersReagentBlockItem>
 {
 
     override fun getAttributeModifier(): Pair<EntityAttribute,EntityAttributeModifier>{
         return Pair(attribute,modifier)
+    }
+
+    override var glint = false
+    override var flavor: String = ""
+    override var flavorDesc: String = ""
+    
+    private val flavorText: MutableText by lazy{
+        makeFlavorText()
+    }
+    
+    private val flavorTextDesc: MutableText by lazy{
+        makeFlavorTextDesc()
+    }
+    
+    private fun makeFlavorText(): MutableText{
+        val id = Registries.ITEM.getId(this)
+        val key = "item.${id.namespace}.${id.path}.flavor"
+        val text = AcText.translatable(key).formatted(Formatting.WHITE, Formatting.ITALIC)
+        if (text.string == key) return AcText.empty()
+        return text
+    }
+    
+    private fun makeFlavorTextDesc(): MutableText{
+        val id = Registries.ITEM.getId(this)
+        val key = "item.${id.namespace}.${id.path}.flavor.desc"
+        val text = AcText.translatable(key).formatted(Formatting.WHITE)
+        if (text.string == key) return AcText.empty()
+        return text
+    }
+
+    override fun appendTooltip(stack: ItemStack, world: World?, tooltip: MutableList<Text>, context: TooltipContext) {
+        super.appendTooltip(stack, world, tooltip, context)
+        addFlavorText(tooltip, context)
+    }
+
+    override fun hasGlint(stack: ItemStack): Boolean {
+        return if (glint) {
+            true
+        } else {
+            super.hasGlint(stack)
+        }
+    }
+    
+    override fun flavorText(): MutableText{
+        return flavorText
+    }
+    override fun flavorDescText(): MutableText{
+        return flavorTextDesc
+    }
+
+    override fun getFlavorItem(): CustomFlavorItem {
+        return this
     }
 
 }
