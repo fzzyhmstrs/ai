d2323c740df314e655ecb872007258612584ff73
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-08-04 12:16:54 -0400

    Update RegisterArmor.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/registry/RegisterArmor.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/registry/RegisterArmor.kt
@@ -26,6 +26,18 @@ object RegisterArmor {
     val AMETRINE_CHESTPLATE = register(ArmorItem(AiConfig.materials.armor.ametrine, ArmorItem.Type.CHESTPLATE,Item.Settings()), "ametrine_chestplate")
     val AMETRINE_LEGGINGS = register(ArmorItem(AiConfig.materials.armor.ametrine, ArmorItem.Type.LEGGINGS,Item.Settings()), "ametrine_leggings")
     val AMETRINE_BOOTS = register(ArmorItem(AiConfig.materials.armor.ametrine, ArmorItem.Type.BOOTS,Item.Settings()),"ametrine_boots")
-
+    val GARNET_HELMET = register(ArmorItem(AiConfig.materials.armor.garnet, ArmorItem.Type.HELMET,Item.Settings()), "garnet_helmet")
+    val GARNET_CHESTPLATE = register(ArmorItem(AiConfig.materials.armor.garnet, ArmorItem.Type.CHESTPLATE,Item.Settings()), "garnet_chestplate")
+    val GARNET_LEGGINGS = register(ArmorItem(AiConfig.materials.armor.garnet, ArmorItem.Type.LEGGINGS,Item.Settings()), "garnet_leggings")
+    val GARNET_BOOTS = register(ArmorItem(AiConfig.materials.armor.garnet, ArmorItem.Type.BOOTS,Item.Settings()),"garnet_boots")
+    val GLOWING_HELMET = register(ArmorItem(AiConfig.materials.armor.glowing, ArmorItem.Type.HELMET,Item.Settings()), "glowing_helmet")
+    val GLOWING_CHESTPLATE = register(ArmorItem(AiConfig.materials.armor.glowing, ArmorItem.Type.CHESTPLATE,Item.Settings()), "glowing_chestplate")
+    val GLOWING_LEGGINGS = register(ArmorItem(AiConfig.materials.armor.glowing, ArmorItem.Type.LEGGINGS,Item.Settings()), "glowing_leggings")
+    val GLOWING_BOOTS = register(ArmorItem(AiConfig.materials.armor.glowing, ArmorItem.Type.BOOTS,Item.Settings()),"glowing_boots")
+    val SHIMMERING_HELMET = register(ArmorItem(AiConfig.materials.armor.shimmering, ArmorItem.Type.HELMET,Item.Settings()), "shimmering_helmet")
+    val SHIMMERING_CHESTPLATE = register(ArmorItem(AiConfig.materials.armor.shimmering, ArmorItem.Type.CHESTPLATE,Item.Settings()), "shimmering_chestplate")
+    val SHIMMERING_LEGGINGS = register(ArmorItem(AiConfig.materials.armor.shimmering, ArmorItem.Type.LEGGINGS,Item.Settings()), "shimmering_leggings")
+    val SHIMMERING_BOOTS = register(ArmorItem(AiConfig.materials.armor.shimmering, ArmorItem.Type.BOOTS,Item.Settings()),"shimmering_boots")
+    
     fun registerAll() {}
 }
