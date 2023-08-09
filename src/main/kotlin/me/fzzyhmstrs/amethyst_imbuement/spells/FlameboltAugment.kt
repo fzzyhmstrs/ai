1487a1525777178e9c05890b0905bef30039c389
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-08-09 14:54:30 -0400

    Update FlameboltAugment.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/spells/FlameboltAugment.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/spells/FlameboltAugment.kt
@@ -70,7 +70,7 @@ class FlameboltAugment: ProjectileAugment(ScepterTier.ONE){
         for (i in 1..count){
             val me = BasicMissileEntity(world, user).burning().color(MissileEntity.ColorData(1f, 0.7f, 0.3f, 0.6666f, 1.2f))
             val direction = user.rotationVec3d
-            me.setVelocity(direction.x,direction.y,direction.z, 2.0f, 0.1f)
+            me.place(user,direction,-0.2, 2f, 0.1f, 0.6)
             me.passEffects(spells,effects,level)
             me.passContext(context)
             list.add(me)
