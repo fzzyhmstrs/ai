e77d92f054de2d8373cb4a7be1099858a515189c
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-08-09 14:55:21 -0400

    Update SoulMissileAugment.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/spells/SoulMissileAugment.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/spells/SoulMissileAugment.kt
@@ -139,7 +139,7 @@ class SoulMissileAugment: ProjectileAugment(ScepterTier.ONE,){
         for (i in 1..count){
             val me = MissileEntity(world, user)
             val direction = user.rotationVec3d
-            me.setVelocity(direction.x,direction.y,direction.z, 2.0f, 0.1f)
+            me.place(user,direction,-0.2, 2f, 0.1f, 0.6)
             me.passEffects(spells,effects,level)
             me.passContext(context)
             list.add(me)
