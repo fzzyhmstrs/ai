12cd89629cd779170482c329f664307434f9e86e
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-09-27 11:13:46 -0400

    Create MendingAuraAugment.kt

---
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/spells/tales/MendingAuraAugment.kt
@@ -0,0 +1,47 @@
+package me.fzzyhmstrs.amethyst_imbuement.spells.tales
+
+import me.fzzyhmstrs.amethyst_core.modifier_util.AugmentConsumer
+import me.fzzyhmstrs.amethyst_core.modifier_util.AugmentEffect
+import me.fzzyhmstrs.amethyst_core.scepter_util.ScepterTier
+import me.fzzyhmstrs.amethyst_core.scepter_util.SpellType
+import me.fzzyhmstrs.amethyst_core.scepter_util.augments.AugmentDatapoint
+import me.fzzyhmstrs.amethyst_core.scepter_util.augments.MinorSupportAugment
+import me.fzzyhmstrs.amethyst_imbuement.item.book.BookOfTalesItem
+import net.minecraft.entity.Entity
+import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.effect.StatusEffectInstance
+import net.minecraft.entity.effect.StatusEffects
+import net.minecraft.item.Items
+import net.minecraft.sound.SoundCategory
+import net.minecraft.sound.SoundEvent
+import net.minecraft.sound.SoundEvents
+import net.minecraft.world.World
+import kotlin.math.max
+
+class MendingAuraAugment: MinorSupportAugment(ScepterTier.THREE,7){
+
+    override val baseEffect: AugmentEffect
+        get() = super.baseEffect.withAmplifier(-1,1)
+
+    override fun augmentStat(imbueLevel: Int): AugmentDatapoint {
+        return AugmentDatapoint(SpellType.GRACE, 12000, 750,
+            22, imbueLevel, 100, BookOfTalesItem.TALES_TIER, Items.GOLD_BLOCK)
+    }
+
+    override fun supportEffect(
+        world: World,
+        target: Entity?,
+        user: LivingEntity,
+        level: Int,
+        effects: AugmentEffect
+    ): Boolean {
+        user.addStatusEffect(StatusEffectInstance(RegisterStatus.MENDING_AURA, -1, (effects.amplifier(level)/3)))
+        effects.accept(user, AugmentConsumer.Type.BENEFICIAL)
+        world.playSound(null, user.blockPos, soundEvent(), SoundCategory.PLAYERS, 0.6F, 1.2F)
+        return true
+    }
+
+    override fun soundEvent(): SoundEvent {
+        return SoundEvents.BLOCK_RESPAWN_ANCHOR_SET_SPAWN
+    }
+}
