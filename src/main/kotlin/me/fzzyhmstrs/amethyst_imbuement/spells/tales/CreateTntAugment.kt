cc7bf9ca1e9090357698d198bc2b06113b53b92a
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-09-27 14:19:29 -0400

    Create CreateTntAugment.kt

---
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/spells/tales/CreateTntAugment.kt
@@ -0,0 +1,30 @@
+package me.fzzyhmstrs.amethyst_imbuement.spells
+
+import me.fzzyhmstrs.amethyst_core.scepter_util.LoreTier
+import me.fzzyhmstrs.amethyst_core.scepter_util.ScepterTier
+import me.fzzyhmstrs.amethyst_core.scepter_util.SpellType
+import me.fzzyhmstrs.amethyst_core.scepter_util.augments.AugmentDatapoint
+import me.fzzyhmstrs.amethyst_core.scepter_util.augments.PlaceItemAugment
+import net.minecraft.item.Items
+import net.minecraft.sound.SoundEvent
+import net.minecraft.sound.SoundEvents
+
+class CreateTntAugment: PlaceItemAugment(ScepterTier.THREE, 1, Items.TNT){
+    override fun augmentStat(imbueLevel: Int): AugmentDatapoint {
+        return AugmentDatapoint(SpellType.WIT,100,50,
+            20,imbueLevel,6, BookOfTalesItem.TALES_TIER, Items.TNT)
+    }
+
+    override fun blockPlacing(hit: BlockHitResult, world: World, user: ServerPlayerEntity, hand: Hand, level: Int, effects: AugmentEffect): Boolean{
+        val bl = super.blockPlacing(hit,world,user,hand.level,effects)
+        if (bl){
+            TntBlock.primeTnt(world,hit.blockPos)
+        }
+        return bl
+    }
+
+    override fun soundEvent(): SoundEvent {
+        return SoundEvents.BLOCK_GRASS_PLACE
+    }
+
+}
