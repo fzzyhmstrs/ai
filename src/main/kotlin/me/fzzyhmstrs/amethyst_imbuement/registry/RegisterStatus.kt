a9d9bb79c5541873256c60811d23dfb6feab93bb
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-09-27 09:13:39 -0400

    Update RegisterStatus.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/registry/RegisterStatus.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/registry/RegisterStatus.kt
@@ -56,6 +56,10 @@ object RegisterStatus {
         .addAttributeModifier(EntityAttributes.GENERIC_ATTACK_SPEED,"0a3a25d6-5ccd-11ee-8c99-0242ac120002", 0.075,EntityAttributeModifier.Operation.MULTIPLY_TOTAL)
         .addAttributeModifier(RegisterAttribute.SPELL_COOLDOWN,"0a3a2842-5ccd-11ee-8c99-0242ac120002", 0.1, EntityAttributeModifier.Operation.MULTIPLY_TOTAL)
         ,"lightning_aura")
+    val ARCANE_AURA = register(ArcaneAuraStatusEffect(StatusEffectCategory.BENEFICIAL,0xDDA0DD)
+        .addAttributeModifier(RegisterAttribute.SPELL_AMPLIFIER,"26613746-5d35-11ee-8c99-0242ac120002", 1.0,EntityAttributeModifier.Operation.ADDITION)
+        .addAttributeModifier(RegisterAttribute.SPELL_DAMAGE,"2661398a-5d35-11ee-8c99-0242ac120002", 0.15, EntityAttributeModifier.Operation.MULTIPLY_TOTAL)
+        ,"arcane_aura")
 
     fun registerAll(){}
 }
