a8df706d5a179806c52439d08362e5c36ae17b4b
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-10-10 11:59:52 -0400

    Update ForcefieldAugment.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/spells/ForcefieldAugment.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/spells/ForcefieldAugment.kt
@@ -23,8 +23,7 @@ import net.minecraft.world.World
 class ForcefieldAugment: MiscAugment(ScepterTier.TWO,6){
 
     private val offset = intArrayOf(-2,2)
-    override val baseEffect: AugmentEffect
-        get() = super.baseEffect.withDuration(1800,200)
+    override val baseEffect: AugmentEffect = super.baseEffect.withDuration(1800,200)
 
     override fun augmentStat(imbueLevel: Int): AugmentDatapoint {
         return AugmentDatapoint(SpellType.WIT, PerLvlI(620,-20),135,
