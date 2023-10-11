e076d9b77bd0424ae9686232846f767304131379
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-10-11 13:56:32 -0400

    Update RegisterEnchantment.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/registry/RegisterEnchantment.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/registry/RegisterEnchantment.kt
@@ -177,6 +177,7 @@ object RegisterEnchantment {
     val CHAIN_LIGHTNING = ChainLightningAugment().register("chain_lightning")
     val CHAOS_BOLT = ChaosBoltAugment().register("chaos_bolt")
     val CREATE_TNT = CreateTntAugment().register("create_tnt")
+    val ENERGY_BLADE = EnergyBladeAugment().register("energy_blade")
     val FULL_HEAL = FullHealAugment().register("full_heal")
     val ICE_BURST = IceBurstAugment().register("ice_burst")
     val LIGHTNING_AURA = LightningAuraAugment().register("lightning_aura")
