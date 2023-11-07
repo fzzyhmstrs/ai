c0503f51f4f5d805fe0525629d4636425698dc76
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-11-07 11:47:38 -0500

    Update SoulwovenArmorItem.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/armor/SoulwovenArmorItem.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/item/armor/SoulwovenArmorItem.kt
@@ -16,6 +16,27 @@ import net.minecraft.world.World
 
 class SoulwovenArmorItem(material: ArmorMaterial, type: Type, settings: Settings) : ArmorItem(material, type, settings), Modifiable {
 
+    private val attributeModifiers: Multimap<EntityAttribute, EntityAttributeModifier> by lazy {
+        val map: ArrayListMultimap<EntityAttribute, EntityAttributeModifier> = ArrayListMultimap.create()
+        map.putAll(super.getAttributeModifiers(type.equipmentSlot))
+        if (SpChecker.spellPowerLoaded) {
+            val attribute = Registries.ATTRIBUTE.get(Identifier("spell_power","soul"))
+            if (attribute != null) {
+                val MODIFIERS: EnumMap<Type, UUID> = Util.make(EnumMap<Type, UUID>(Type::class.java)) { uuidMap: EnumMap<Type, UUID> ->
+                    uuidMap[Type.BOOTS] = UUID.fromString("6959f0e8-7d8c-11ee-b962-0242ac120002")
+                    uuidMap[Type.LEGGINGS] = UUID.fromString("6959f3ea-7d8c-11ee-b962-0242ac120002")
+                    uuidMap[Type.CHESTPLATE] = UUID.fromString("6959f53e-7d8c-11ee-b962-0242ac120002")
+                    uuidMap[Type.HELMET] = UUID.fromString("6959fab6-7d8c-11ee-b962-0242ac120002")
+                }
+                val uUID = MODIFIERS[type] ?: UUID.randomUUID()
+                map.put(attribute, EntityAttributeModifier(uUID,"soul_power", 2.0, EntityAttributeModifier.Operation.ADDITION))
+            }
+        }
+        map
+    }
+
+    private 
+    
     private val flavorText: MutableText by lazy{
         FlavorHelper.makeFlavorText(this)
     }
@@ -38,5 +59,12 @@ class SoulwovenArmorItem(material: ArmorMaterial, type: Type, settings: Settings
         if (type == ModifierHelper.getType())
             return mutableListOf(RegisterModifier.ensouledModifiers.random().modifierId)
         return super.defaultModifiers(type)
+    }
+
+    override fun getAttributeModifiers(slot: EquipmentSlot): Multimap<EntityAttribute, EntityAttributeModifier>{
+        if (slot == type.equipmentSlot) {
+                return this.attributeModifiers
+        }
+        return super.getAttributeModifiers(slot)
     }
 }
