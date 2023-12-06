a7dd9904ffce4366a48611655dabc687e9b47bbb
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-12-05 21:25:17 -0500

    1.19-40, fix environment annotations.

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/registry/RegisterKeybindServer.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/registry/RegisterKeybindServer.kt
@@ -1,8 +1,6 @@
 package me.fzzyhmstrs.amethyst_imbuement.registry
 
 import me.fzzyhmstrs.amethyst_imbuement.AI
-import net.fabricmc.api.EnvType
-import net.fabricmc.api.Environment
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking
 import net.minecraft.util.Identifier
 import java.util.*
@@ -10,7 +8,6 @@ import java.util.*
 object RegisterKeybindServer {
 
     private val VEIN_MINER_PACKET = Identifier(AI.MOD_ID, "vein_miner_packet")
-    @Environment(value = EnvType.SERVER)
     private val veinMiners: MutableMap<UUID,Boolean> = mutableMapOf()
 
     fun registerServer(){
