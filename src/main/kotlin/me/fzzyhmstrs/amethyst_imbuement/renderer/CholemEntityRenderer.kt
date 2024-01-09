2d7ca9989608e211eb19057d594775bf8d42d8f9
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2024-01-09 14:42:33 -0500

    Update CholemEntityRenderer.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/renderer/CholemEntityRenderer.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/renderer/CholemEntityRenderer.kt
@@ -3,13 +3,13 @@ package me.fzzyhmstrs.amethyst_imbuement.renderer
 import me.fzzyhmstrs.amethyst_imbuement.entity.living.CholemEntity
 import me.fzzyhmstrs.amethyst_imbuement.model.CholemEntityModel
 import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterRenderer
+import me.fzzyhmstrs.fzzy_core.coding_util.compat.FzzyRotation
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
 import net.minecraft.client.render.entity.EntityRendererFactory
 import net.minecraft.client.render.entity.MobEntityRenderer
 import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.util.Identifier
-import net.minecraft.util.math.RotationAxis
 import kotlin.math.abs
 
 @Environment(value = EnvType.CLIENT)
@@ -36,6 +36,6 @@ class CholemEntityRenderer(context: EntityRendererFactory.Context) :
         }
         val j = cholemEntity.limbAnimator.getPos(h) + 6.0f
         val k = (abs(j % 13.0f - 6.5f) - 3.25f) / 3.25f
-        matrixStack.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(6.5f * k))
+        matrixStack.multiply(FzzyRotation.POSITIVE_Z.degrees(6.5f * k))
     }
 }
