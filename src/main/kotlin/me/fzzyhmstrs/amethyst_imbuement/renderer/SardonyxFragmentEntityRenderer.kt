ce7ebe4fd784ea069bd0a79c6d529223f6b7243e
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2024-01-09 14:41:51 -0500

    Update SardonyxFragmentEntityRenderer.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/renderer/SardonyxFragmentEntityRenderer.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/renderer/SardonyxFragmentEntityRenderer.kt
@@ -4,13 +4,13 @@ import me.fzzyhmstrs.amethyst_imbuement.AI
 import me.fzzyhmstrs.amethyst_imbuement.entity.monster.SardonyxFragmentEntity
 import me.fzzyhmstrs.amethyst_imbuement.model.SardonyxFragmentEntityModel
 import me.fzzyhmstrs.amethyst_imbuement.registry.RegisterRenderer
+import me.fzzyhmstrs.fzzy_core.coding_util.compat.FzzyRotation
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
 import net.minecraft.client.render.entity.EntityRendererFactory
 import net.minecraft.client.render.entity.MobEntityRenderer
 import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.util.Identifier
-import net.minecraft.util.math.RotationAxis
 import kotlin.math.abs
 
 @Environment(value = EnvType.CLIENT)
@@ -39,6 +39,6 @@ class SardonyxFragmentEntityRenderer(context: EntityRendererFactory.Context) :
         }
         val j = sardonyxFragmentEntity.limbAnimator.getPos(h) + 6.0f
         val k = (abs(j % 13.0f - 6.5f) - 3.25f) / 3.25f
-        matrixStack.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(6.5f * k))
+        matrixStack.multiply(FzzyRotation.POSITIVE_Z.degrees(6.5f * k))
     }
 }
