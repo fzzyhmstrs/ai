ed201286ebfc688cac79a2263a60e9bbf20a07f6
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2024-02-22 12:26:11 -0500

    Create chickenform.kt

---
+++ src/main/resources/assets/amethyst_imbuement/patchouli_books/glistering_tome/en_us/entries/spell_book/chickenform.kt
@@ -0,0 +1,17 @@
+{
+"name": "Chickenform",
+"icon": "amethyst_imbuement:a_scepter_so_fowl",
+"category": "amethyst_imbuement:spell_book",
+"advancement": "amethyst_imbuement:spells/chickenform",
+"pages": [
+{
+"type": "amethyst_imbuement:spell_text",
+"spell": "amethyst_imbuement:chickenform"
+},
+{
+"type": "patchouli:spotlight",
+"item": "amethyst_imbuement:a_scepter_so_fowl",
+"text": "The only way to obtain Chickenform is to find A Scepter So Fowl."       
+}
+]
+}
